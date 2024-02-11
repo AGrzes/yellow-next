a7672dafee334826f4d076895cf01b4d5f327b7c
Grzegorz Abramczyk <abramczykg@gmail.com>
2024-02-11 09:30:24 +0100

    Updated CLI approach

--- core/src/cli/index.ts
+++ core/src/cli/index.ts
@@ -1,18 +1,17 @@
 import { Command } from 'commander'
 import { Container, ContainerModule } from 'inversify'
 
-export const ROOT_COMMAND = Symbol('ROOT_COMMAND')
-
 export const cliModule = new ContainerModule((bind) => {
-  bind(ROOT_COMMAND)
+  bind(Command)
     .toDynamicValue(() => new Command())
     .inSingletonScope()
+    .whenTargetNamed('root')
 })
 
 export default async function entrypoint(container: Container, argv?: readonly string[]) {
   if (container.isBound(Command)) {
     await container.getAllAsync(Command)
   }
-  const program = await container.getAsync<Command>(ROOT_COMMAND)
+  const program = await container.getNamedAsync<Command>(Command, 'root')
   await program.parseAsync(argv)
 }
--- core/test/cli/index.ts
+++ core/test/cli/index.ts
@@ -4,14 +4,14 @@ import { Container, ContainerModule } from 'inversify'
 import 'mocha'
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
-import entrypoint, { ROOT_COMMAND, cliModule } from '../../src/cli/index.js'
+import entrypoint, { cliModule } from '../../src/cli/index.js'
 const expect = chai.use(sinonChai).expect
 
 describe('cli', () => {
   it('should be able to create a container', async () => {
     const container = new Container()
     container.load(cliModule)
-    expect(container.isBound(ROOT_COMMAND)).to.be.true
+    expect(container.isBoundNamed(Command, 'root')).to.be.true
   })
 
   it('should be able to run the entrypoint', async () => {
@@ -25,22 +25,20 @@ describe('cli', () => {
     container.load(
       cliModule,
       new ContainerModule((bind) => {
-        bind(Command).toDynamicValue((context) => {
-          const parent = context.container.get<Command>(ROOT_COMMAND)
-          const command = new Command('child')
-          command.action(action)
-          parent.exitOverride()
-          parent.addCommand(command)
-          return command
-        })
+        bind(Command)
+          .toDynamicValue((context) => {
+            const parent = context.container.getNamed(Command, 'root')
+            const command = new Command('child')
+            command.action(action)
+            parent.exitOverride()
+            parent.addCommand(command)
+            return command
+          })
+          .inSingletonScope()
+          .whenTargetNamed('child')
       })
     )
-    try {
-      await entrypoint(container, ['', '', 'child'])
-
-      expect(action).to.have.been.calledOnce
-    } catch (e) {
-      console.log(e)
-    }
+    await entrypoint(container, ['', '', 'child'])
+    expect(action).to.have.been.calledOnce
   })
 })
