531a130e3a34e4239a3dfcdd4ca76f1447b8df06
Grzegorz Abramczyk <abramczykg@gmail.com>
2024-02-11 09:31:46 +0100

    Updated program setup

--- core/src/container/index.ts
+++ core/src/container/index.ts
@@ -1,8 +1,10 @@
 import { Container } from 'inversify'
+import { adbsModule } from '../adbs/module.js'
 import { cliModule } from '../cli/index.js'
+import { serverCliModule } from '../cli/server.js'
 
 export default async function createContainer() {
   const container = new Container()
-  container.load(cliModule)
+  container.load(cliModule, serverCliModule, adbsModule)
   return container
 }
--- core/src/index.ts
+++ core/src/index.ts
@@ -1,4 +1,6 @@
 /* c8 ignore start */
+import 'reflect-metadata'
+
 import entrypoint from './cli/index.js'
 import createContainer from './container/index.js'
 entrypoint(await createContainer(), process.argv)
