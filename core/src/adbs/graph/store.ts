2469ee606452d94d12f2a85aba06ad733cd51d16
Grzegorz Abramczyk <abramczykg@gmail.com>
2024-02-13 15:12:12 +0100

    Graph store

---
+++ core/src/adbs/graph/store.ts
@@ -0,0 +1,55 @@
+import debug from 'debug'
+import { DataFactory, Quad_Graph, Store, Triple } from 'n3'
+import { BehaviorSubject, Observable, PartialObserver, debounceTime, map } from 'rxjs'
+import { ChangeEvent, DeleteEvent, MoveEvent, UpdateEvent } from '../model.js'
+
+const log = debug('yellow:adbs:graph:store')
+
+export class GraphStore {
+  public readonly store: Store = new Store()
+  private storeSubject = new BehaviorSubject<Store>(this.store)
+  public get observableStore(): Observable<Store> {
+    return this.storeSubject
+  }
+  public readonly observableUnion: Observable<Store> = this.observableStore.pipe(
+    debounceTime(100),
+    map(
+      (store) =>
+        new Store(
+          store
+            .getQuads(null, null, null, null)
+            .map(({ subject, predicate, object }) => DataFactory.triple(subject, predicate, object))
+        )
+    )
+  )
+
+  constructor() {}
+
+  public readonly observer: PartialObserver<ChangeEvent<Triple[], Quad_Graph>> = {
+    next: async (event) => {
+      log(`event ${event.kind} : ${event.key.value}`)
+      if (event.kind === 'update') {
+        const { content: triples, key: graph, hint } = event as UpdateEvent<Triple[], Quad_Graph>
+        if (hint !== 'add') {
+          this.store.removeQuads(this.store.getQuads(null, null, null, graph))
+        }
+        this.store.addQuads(
+          triples.map(({ subject, predicate, object }) => DataFactory.quad(subject, predicate, object, graph))
+        )
+      } else if (event.kind === 'delete') {
+        const { key: graph } = event as DeleteEvent<Triple[], Quad_Graph>
+        this.store.removeQuads(this.store.getQuads(null, null, null, graph))
+      } else if (event.kind === 'move') {
+        const { key: graph, newKey: newGraph } = event as MoveEvent<Triple[], Quad_Graph>
+        this.store.removeQuads(this.store.getQuads(null, null, null, newGraph))
+        this.store.addQuads(
+          this.store
+            .getQuads(null, null, null, graph)
+            .map(({ subject, predicate, object }) => DataFactory.quad(subject, predicate, object, newGraph))
+        )
+        this.store.removeQuads(this.store.getQuads(null, null, null, graph))
+      }
+      this.storeSubject.next(this.store)
+    },
+  }
+}
---
+++ core/test/adbs/graph/store.ts
@@ -0,0 +1,107 @@
+import 'mocha'
+
+import chai from 'chai'
+import { DataFactory, Quad_Graph, Triple } from 'n3'
+import { firstValueFrom, lastValueFrom, take } from 'rxjs'
+import sinonChai from 'sinon-chai'
+import { GraphStore } from '../../../src/adbs/graph/store.js'
+import { MoveEvent, UpdateEvent } from '../../../src/adbs/model.js'
+
+const { expect } = chai.use(sinonChai)
+const triple = DataFactory.triple(
+  DataFactory.namedNode('urn:s'),
+  DataFactory.namedNode('urn:p'),
+  DataFactory.namedNode('urn:o')
+)
+const quad = DataFactory.quad(
+  DataFactory.namedNode('urn:s'),
+  DataFactory.namedNode('urn:p'),
+  DataFactory.namedNode('urn:o'),
+  DataFactory.namedNode('urn:graph')
+)
+describe('adbs', () => {
+  describe('graph', () => {
+    describe('mapper', () => {
+      describe('GraphStore', () => {
+        it('should react to updates', async () => {
+          const store = new GraphStore()
+          store.observer.next!({
+            key: DataFactory.namedNode('urn:graph'),
+            kind: 'update',
+            content: [triple],
+            hint: 'add',
+          } as UpdateEvent<Triple[], Quad_Graph>)
+          const storeValue = await firstValueFrom(store.observableStore)
+          expect(storeValue.getQuads(null, null, null, DataFactory.namedNode('urn:graph'))).to.deep.equal([quad])
+        })
+        it('should react to deletes', async () => {
+          const store = new GraphStore()
+          const lastValue = lastValueFrom(store.observableStore.pipe(take(2)))
+          store.observer.next!({
+            key: DataFactory.namedNode('urn:graph'),
+            kind: 'update',
+            content: [triple],
+            hint: 'add',
+          } as UpdateEvent<Triple[], Quad_Graph>)
+          store.observer.next!({
+            key: DataFactory.namedNode('urn:graph'),
+            kind: 'delete',
+          })
+          const storeValue = await lastValue
+          expect(storeValue.getQuads(null, null, null, DataFactory.namedNode('urn:graph'))).to.deep.equal([])
+        })
+        it('should react to moves', async () => {
+          const store = new GraphStore()
+          store.observer.next!({
+            key: DataFactory.namedNode('urn:oldGraph'),
+            kind: 'update',
+            content: [triple],
+            hint: 'add',
+          } as UpdateEvent<Triple[], Quad_Graph>)
+          store.observer.next!({
+            key: DataFactory.namedNode('urn:oldGraph'),
+            kind: 'move',
+            newKey: DataFactory.namedNode('urn:graph'),
+          } as MoveEvent<Triple[], Quad_Graph>)
+          const storeValue = await firstValueFrom(store.observableStore)
+          expect(storeValue.getQuads(null, null, null, DataFactory.namedNode('urn:oldGraph'))).to.deep.equal([])
+          expect(storeValue.getQuads(null, null, null, DataFactory.namedNode('urn:graph'))).to.deep.equal([quad])
+        })
+        it('should remove old quads on update', async () => {
+          const store = new GraphStore()
+          store.observer.next!({
+            key: DataFactory.namedNode('urn:graph'),
+            kind: 'update',
+            content: [triple],
+            hint: 'add',
+          } as UpdateEvent<Triple[], Quad_Graph>)
+          store.observer.next!({
+            key: DataFactory.namedNode('urn:graph'),
+            kind: 'update',
+            content: [],
+            hint: 'replace',
+          } as UpdateEvent<Triple[], Quad_Graph>)
+          const storeValue = await firstValueFrom(store.observableStore)
+          expect(storeValue.getQuads(null, null, null, DataFactory.namedNode('urn:graph'))).to.deep.equal([])
+        })
+        it('should provide unin store', async () => {
+          const store = new GraphStore()
+          store.observer.next!({
+            key: DataFactory.namedNode('urn:graph'),
+            kind: 'update',
+            content: [triple],
+            hint: 'add',
+          } as UpdateEvent<Triple[], Quad_Graph>)
+          store.observer.next!({
+            key: DataFactory.namedNode('urn:graph2'),
+            kind: 'update',
+            content: [triple],
+            hint: 'add',
+          } as UpdateEvent<Triple[], Quad_Graph>)
+          const storeValue = await firstValueFrom(store.observableUnion)
+          expect(storeValue.getQuads(null, null, null, null)).to.have.length(1)
+        })
+      })
+    })
+  })
+})
