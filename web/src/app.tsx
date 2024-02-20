4891f480fa9600770b0b14d6a7fa7e3cd6694f9b
Grzegorz Abramczyk <abramczykg@gmail.com>
2024-02-20 22:02:34 +0100

    Support for various types of documents

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,12 +1,26 @@
 import React, { FC, useEffect, useState } from 'react'
 import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom'
 
+const documentExtensions = ['.mdx', '.md', '.tsx']
+
 function Document({ path, ...other }: { path: string }) {
   const [Component, setComponent] = useState<FC>()
   useEffect(() => {
     const loadComponent = async () => {
+      let component: FC
       // @ts-ignore
-      const component = (await import(/* webpackInclude: /\.mdx$/ */ `@documents/${path}.mdx`)).default
+      for (const extension of documentExtensions) {
+        try {
+          component = (await import(/* webpackInclude: /\.(mdx|tsx|md)$/ */ `@documents/${path}${extension}`)).default
+          break
+        } catch (error) {
+          console.error(error)
+        }
+      }
+
+      if (!component) {
+        component = () => <div>Not found</div>
+      }
       setComponent(() => component)
     }
     loadComponent().catch(console.error)
