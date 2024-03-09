a43f6594af113d0b958c9faf90b0bae1c8eb191a
Grzegorz Abramczyk <abramczykg@gmail.com>
2024-03-09 20:51:45 +0100

    Dynamic data access

---
+++ shared/src/dynamic.ts
@@ -0,0 +1,79 @@
+import { BlankNode, DataFactory, NamedNode, Store, Term } from 'n3'
+
+export interface ClassOptions {
+  name: string
+  iri: string
+  properties: PropertyOptions[]
+}
+
+export interface PropertyOptions {
+  name: string
+  iri: string
+  reverse?: boolean
+}
+
+export interface ModelOptions {
+  classes: ClassOptions[]
+}
+
+interface TypedNode {
+  iri: NamedNode | BlankNode
+  classes: ClassOptions[]
+}
+
+export class Model {
+  private handler: ProxyHandler<TypedNode>
+
+  constructor(
+    private store: Store,
+    private options: ModelOptions
+  ) {
+    this.handler = {
+      get: (target, property, receiver) => {
+        const propertyOptions = target.classes.flatMap((c) => c.properties).find((p) => p.name === property)
+        if (propertyOptions) {
+          const values = propertyOptions.reverse
+            ? this.store
+                .getSubjects(DataFactory.namedNode(propertyOptions.iri), target.iri, null)
+                .map((iri: NamedNode | BlankNode) => this.proxy(iri))
+            : this.store
+                .getObjects(target.iri, DataFactory.namedNode(propertyOptions.iri), null)
+                .map((iri: NamedNode | BlankNode) => this.proxy(iri))
+          if (values.length === 1) {
+            return values[0]
+          } else {
+            return values
+          }
+        }
+      },
+    }
+  }
+
+  private proxy(iri: Term): any {
+    if (iri.termType === 'Literal') {
+      return iri.value
+    } else {
+      const classes = this.store
+        .getObjects(iri, DataFactory.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), null)
+        .map((iri: NamedNode | BlankNode) => this.options.classes.find((c) => c.iri === iri.value))
+        .filter((c) => c)
+      return new Proxy({ iri, classes }, this.handler)
+    }
+  }
+
+  all(name: string) {
+    const classOptions = this.options.classes.find((c) => c.name === name)
+
+    if (classOptions) {
+      return this.store
+        .getSubjects(
+          DataFactory.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+          DataFactory.namedNode(classOptions.iri),
+          null
+        )
+        .map((iri: NamedNode | BlankNode) => this.proxy(iri))
+    } else {
+      throw new Error(`Class ${name} not found`)
+    }
+  }
+}
---
+++ shared/test/dynamic.ts
@@ -0,0 +1,92 @@
+import { expect } from 'chai'
+import { readFile } from 'fs/promises'
+import jsonld from 'jsonld'
+import 'mocha'
+import { Quad, Store } from 'n3'
+import { Model, ModelOptions } from '../src/dynamic.js'
+
+const modelOptions: ModelOptions = {
+  classes: [
+    {
+      iri: 'http://agrzes.pl/books#Book',
+      name: 'Book',
+      properties: [
+        {
+          iri: 'http://agrzes.pl/books#Book/pages',
+          name: 'pages',
+        },
+        {
+          iri: 'http://agrzes.pl/books#Book/released',
+          name: 'released',
+        },
+        {
+          iri: 'http://www.w3.org/2000/01/rdf-schema#label',
+          name: 'title',
+        },
+        {
+          iri: 'http://agrzes.pl/books#Book/author',
+          name: 'author',
+        },
+        {
+          iri: 'http://agrzes.pl/books#Book/series',
+          name: 'series',
+        },
+      ],
+    },
+    {
+      name: 'Author',
+      iri: 'http://agrzes.pl/books#Author',
+      properties: [
+        {
+          iri: 'http://www.w3.org/2000/01/rdf-schema#label',
+          name: 'name',
+        },
+        {
+          name: 'books',
+          iri: 'http://agrzes.pl/books#Book/author',
+          reverse: true,
+        },
+      ],
+    },
+    {
+      name: 'Series',
+      iri: 'http://agrzes.pl/books#Series',
+      properties: [
+        {
+          iri: 'http://www.w3.org/2000/01/rdf-schema#label',
+          name: 'name',
+        },
+        {
+          name: 'books',
+          iri: 'http://agrzes.pl/books#Book/series',
+          reverse: true,
+        },
+      ],
+    },
+  ],
+}
+
+const store = new Store((await jsonld.toRDF(JSON.parse(await readFile('test/books.jsonld', 'utf-8')))) as Quad[])
+describe.only('Model', () => {
+  it('should list all books', () => {
+    const model = new Model(store, modelOptions)
+    const books = model.all('Book')
+    expect(books).to.have.lengthOf(2)
+    expect(books[0].title).to.equal('B1')
+    expect(books[1].title).to.equal('B2')
+  })
+  it('should navigate to author', () => {
+    const model = new Model(store, modelOptions)
+    const books = model.all('Book')
+    expect(books[0].author.name).to.equal('A1')
+  })
+  it('should navigate back book', () => {
+    const model = new Model(store, modelOptions)
+    const books = model.all('Book')
+    expect(books[0].author.books[0].title).to.equal('B1')
+  })
+  it('should fail on missing class', () => {
+    const model = new Model(store, modelOptions)
+    expect(() => model.all('Bookz')).to.throw()
+  })
+})
