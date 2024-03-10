6b2cce123ef9f7fc7e155cdd5cb6911bb03fcada
Grzegorz Abramczyk <abramczykg@gmail.com>
2024-03-10 23:17:41 +0100

    Semantic model

---
+++ shared/src/access/dynamic/semantic.ts
@@ -0,0 +1,71 @@
+import { RDF, RDFS } from '@inrupt/vocab-common-rdf'
+import { DataFactory, Store } from 'n3'
+import { ClassOptions, ModelOptions, PropertyOptions } from './model.js'
+
+export const CLASS_TYPE = DataFactory.namedNode('agrzes:yellow-next:dynamic:Class')
+export const CLASS_NAME = DataFactory.namedNode('agrzes:yellow-next:dynamic:Class:name')
+export const PROPERTY_NAME = DataFactory.namedNode('agrzes:yellow-next:dynamic:Property:name')
+export const PROPERTY_REVERSE_NAME = DataFactory.namedNode('agrzes:yellow-next:dynamic:Property:reverse_name')
+
+export class SemanticPropertyOptions implements PropertyOptions {
+  constructor(
+    private store: Store,
+    public iri: string,
+    public reverse: boolean
+  ) {}
+
+  private get forwardName(): string {
+    return (
+      this.store.getObjects(DataFactory.namedNode(this.iri), PROPERTY_NAME, null)[0]?.value ||
+      this.store.getObjects(DataFactory.namedNode(this.iri), RDFS.label, null)[0]?.value ||
+      this.iri
+    )
+  }
+
+  get name(): string {
+    if (this.reverse) {
+      return (
+        this.store.getObjects(DataFactory.namedNode(this.iri), PROPERTY_REVERSE_NAME, null)[0]?.value ||
+        `^${this.forwardName}`
+      )
+    } else {
+      return this.forwardName
+    }
+  }
+}
+
+export class SemanticClassOptions implements ClassOptions {
+  constructor(
+    private store: Store,
+    public iri: string
+  ) {}
+
+  get name(): string {
+    return (
+      this.store.getObjects(DataFactory.namedNode(this.iri), CLASS_NAME, null)[0]?.value ||
+      this.store.getObjects(DataFactory.namedNode(this.iri), RDFS.label, null)[0]?.value ||
+      this.iri
+    )
+  }
+
+  get properties(): PropertyOptions[] {
+    return [
+      ...this.store
+        .getObjects(DataFactory.namedNode(this.iri), RDFS.domain, null)
+        .map((iri) => new SemanticPropertyOptions(this.store, iri.value, false)),
+      ...this.store
+        .getObjects(DataFactory.namedNode(this.iri), RDFS.range, null)
+        .map((iri) => new SemanticPropertyOptions(this.store, iri.value, true)),
+    ]
+  }
+}
+
+export class SemanticModelOptions implements ModelOptions {
+  constructor(private store: Store) {}
+
+  get classes(): ClassOptions[] {
+    return this.store
+      .getSubjects(RDF.type, CLASS_TYPE, null)
+      .map((iri) => new SemanticClassOptions(this.store, iri.value))
+  }
+}
---
+++ shared/test/access/dynamic/semantic.ts
@@ -0,0 +1,105 @@
+import { RDF } from '@inrupt/vocab-common-rdf'
+import { expect } from 'chai'
+import 'mocha'
+import { DataFactory, Quad, Store } from 'n3'
+import {
+  CLASS_NAME,
+  CLASS_TYPE,
+  PROPERTY_NAME,
+  PROPERTY_REVERSE_NAME,
+  SemanticClassOptions,
+  SemanticModelOptions,
+  SemanticPropertyOptions,
+} from '../../../src/access/dynamic/semantic.js'
+
+const BOOK = DataFactory.namedNode('http://agrzes.pl/books#Book')
+const BOOK_TITLE = DataFactory.namedNode('http://agrzes.pl/books#Book:title')
+
+describe('access', () => {
+  describe('dynamic', () => {
+    describe('semantic', () => {
+      describe('SemanticModelOptions', () => {
+        it('should create classes from store', () => {
+          const store = new Store<Quad>()
+          store.addQuad(BOOK, DataFactory.namedNode(RDF.type), CLASS_TYPE)
+          const modelOptions = new SemanticModelOptions(store)
+          expect(modelOptions.classes).to.have.length(1)
+          expect(modelOptions.classes[0].iri).to.equal('http://agrzes.pl/books#Book')
+        })
+      })
+      describe('SemanticClassOptions', () => {
+        it('should return class name', () => {
+          const store = new Store<Quad>()
+          store.addQuad(BOOK, CLASS_NAME, DataFactory.literal('Book'))
+          const classOptions = new SemanticClassOptions(store, BOOK.value)
+          expect(classOptions).to.have.property('name', 'Book')
+        })
+        it('should return class name based on label', () => {
+          const store = new Store<Quad>()
+          store.addQuad(
+            BOOK,
+            DataFactory.namedNode('http://www.w3.org/2000/01/rdf-schema#label'),
+            DataFactory.literal('Book')
+          )
+          const classOptions = new SemanticClassOptions(store, BOOK.value)
+          expect(classOptions).to.have.property('name', 'Book')
+        })
+        it('should use iri as class name if no name or label is present', () => {
+          const store = new Store<Quad>()
+          const classOptions = new SemanticClassOptions(store, BOOK.value)
+          expect(classOptions).to.have.property('name', 'http://agrzes.pl/books#Book')
+        })
+        it('should return properties', () => {
+          const store = new Store<Quad>()
+          store.addQuad(BOOK, DataFactory.namedNode('http://www.w3.org/2000/01/rdf-schema#domain'), BOOK_TITLE)
+          const classOptions = new SemanticClassOptions(store, BOOK.value)
+          expect(classOptions.properties).to.have.length(1)
+          expect(classOptions.properties[0].iri).to.equal(BOOK_TITLE.value)
+        })
+        it('should return reverse properties', () => {
+          const store = new Store<Quad>()
+          store.addQuad(BOOK, DataFactory.namedNode('http://www.w3.org/2000/01/rdf-schema#range'), BOOK_TITLE)
+          const classOptions = new SemanticClassOptions(store, BOOK.value)
+          expect(classOptions.properties).to.have.length(1)
+          expect(classOptions.properties[0].iri).to.equal(BOOK_TITLE.value)
+          expect(classOptions.properties[0].reverse).to.be.true
+        })
+      })
+      describe('SemanticPropertyOptions', () => {
+        it('should return property name', () => {
+          const store = new Store<Quad>()
+          store.addQuad(BOOK_TITLE, PROPERTY_NAME, DataFactory.literal('title'))
+          const propertyOptions = new SemanticPropertyOptions(store, BOOK_TITLE.value, false)
+          expect(propertyOptions).to.have.property('name', 'title')
+        })
+        it('should return property name based on label', () => {
+          const store = new Store<Quad>()
+          store.addQuad(
+            BOOK_TITLE,
+            DataFactory.namedNode('http://www.w3.org/2000/01/rdf-schema#label'),
+            DataFactory.literal('title')
+          )
+          const propertyOptions = new SemanticPropertyOptions(store, BOOK_TITLE.value, false)
+          expect(propertyOptions).to.have.property('name', 'title')
+        })
+        it('should use iri as property name if no name or label is present', () => {
+          const store = new Store<Quad>()
+          const propertyOptions = new SemanticPropertyOptions(store, BOOK_TITLE.value, false)
+          expect(propertyOptions).to.have.property('name', 'http://agrzes.pl/books#Book:title')
+        })
+        it('should return reverse property name', () => {
+          const store = new Store<Quad>()
+          store.addQuad(BOOK_TITLE, PROPERTY_REVERSE_NAME, DataFactory.literal('title'))
+          const propertyOptions = new SemanticPropertyOptions(store, BOOK_TITLE.value, true)
+          expect(propertyOptions).to.have.property('name', 'title')
+        })
+        it('should return reverse property name if not defined directly', () => {
+          const store = new Store<Quad>()
+          store.addQuad(BOOK_TITLE, PROPERTY_NAME, DataFactory.literal('title'))
+          const propertyOptions = new SemanticPropertyOptions(store, BOOK_TITLE.value, true)
+          expect(propertyOptions).to.have.property('name', '^title')
+        })
+      })
+    })
+  })
+})
