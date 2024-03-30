4eae5722dbc5f5fbd54e1c86673ee055e2450bc8
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2024-03-30 21:54:31 +0100

    File source producingg method to access content

--- core/src/adbs/file-source.ts
+++ core/src/adbs/file-source.ts
@@ -1,30 +1,52 @@
 import { watch } from 'chokidar'
+import { readFile } from 'fs/promises'
 import { inject, injectable } from 'inversify'
 import { join } from 'path'
 import { Observable, Subject } from 'rxjs'
 import { ChangeEvent, UpdateEvent } from './model.js'
 
+export type ContentSource = () => Promise<string>
+
 @injectable()
 export class FileSource {
-  constructor(@inject(watch) private createWatcher: typeof watch) {}
+  constructor(
+    @inject(watch) private createWatcher: typeof watch,
+    @inject(readFile) private read: typeof readFile
+  ) {}
 
-  observe(documentDirectory: string): Observable<ChangeEvent<void, string>> {
-    const subject = new Subject<ChangeEvent<void, string>>()
+  observe(documentDirectory: string): Observable<ChangeEvent<ContentSource, string>> {
+    const subject = new Subject<ChangeEvent<ContentSource, string>>()
     const watcher = this.createWatcher('.', { cwd: documentDirectory })
+    const content = (path: string) => async () => this.read(join(documentDirectory, path), 'utf8')
     watcher.on('add', (path) =>
-      subject.next({ key: join(documentDirectory, path), kind: 'update', hint: 'add' } as UpdateEvent<void, string>)
+      subject.next({
+        key: join(documentDirectory, path),
+        kind: 'update',
+        hint: 'add',
+        content: content(path),
+      } as UpdateEvent<ContentSource, string>)
     )
     watcher.on('change', (path) =>
-      subject.next({ key: join(documentDirectory, path), kind: 'update', hint: 'update' } as UpdateEvent<void, string>)
+      subject.next({
+        key: join(documentDirectory, path),
+        kind: 'update',
+        hint: 'update',
+        content: content(path),
+      } as UpdateEvent<ContentSource, string>)
     )
     watcher.on('addDir', (path) =>
-      subject.next({ key: join(documentDirectory, path), kind: 'update', hint: 'add' } as UpdateEvent<void, string>)
+      subject.next({
+        key: join(documentDirectory, path),
+        kind: 'update',
+        hint: 'add',
+        content: () => null,
+      } as UpdateEvent<ContentSource, string>)
     )
     watcher.on('unlink', (path) =>
-      subject.next({ key: join(documentDirectory, path), kind: 'delete' } as ChangeEvent<void, string>)
+      subject.next({ key: join(documentDirectory, path), kind: 'delete' } as ChangeEvent<ContentSource, string>)
     )
     watcher.on('unlinkDir', (path) =>
-      subject.next({ key: join(documentDirectory, path), kind: 'delete' } as ChangeEvent<void, string>)
+      subject.next({ key: join(documentDirectory, path), kind: 'delete' } as ChangeEvent<ContentSource, string>)
     )
     watcher.on('error', (err) => {
       subject.error(err)
--- core/test/adbs/file-source.ts
+++ core/test/adbs/file-source.ts
@@ -3,51 +3,53 @@ import 'reflect-metadata'
 
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
+import chaiSubset from 'chai-subset'
 import { watch } from 'chokidar'
+import { readFile } from 'fs/promises'
 import { firstValueFrom } from 'rxjs'
 import { mock, stub } from 'sinon'
 import sinonChai from 'sinon-chai'
 import { FileSource } from '../../src/adbs/file-source'
-const expect = chai.use(sinonChai).use(chaiAsPromised).expect
+const expect = chai.use(sinonChai).use(chaiAsPromised).use(chaiSubset).expect
 describe('adbs', () => {
   describe('FileSource', () => {
     it('should create watcher', () => {
       const createWatcher: typeof watch = mock().returns({ on: () => {}, close: () => {} })
-      const fileSource = new FileSource(createWatcher)
+      const fileSource = new FileSource(createWatcher, readFile)
       fileSource.observe('cwd')
       expect(createWatcher).to.have.been.calledWithExactly('.', { cwd: 'cwd' })
     })
     it('should react to add event', async () => {
       const on = stub()
       const createWatcher: typeof watch = stub().returns({ on, close: () => {} })
-      const fileSource = new FileSource(createWatcher)
+      const fileSource = new FileSource(createWatcher, readFile)
       const observer = fileSource.observe('cwd')
       const first = firstValueFrom(observer)
       on.getCall(0).args[1]('path')
-      expect(await first).to.deep.equal({ key: 'cwd/path', kind: 'update', hint: 'add' })
+      expect(await first).to.containSubset({ key: 'cwd/path', kind: 'update', hint: 'add' })
     })
     it('should react to change event', async () => {
       const on = stub()
       const createWatcher: typeof watch = stub().returns({ on, close: () => {} })
-      const fileSource = new FileSource(createWatcher)
+      const fileSource = new FileSource(createWatcher, readFile)
       const observer = fileSource.observe('cwd')
       const first = firstValueFrom(observer)
       on.getCall(1).args[1]('path')
-      expect(await first).to.deep.equal({ key: 'cwd/path', kind: 'update', hint: 'update' })
+      expect(await first).to.containSubset({ key: 'cwd/path', kind: 'update', hint: 'update' })
     })
     it('should react to addDir event', async () => {
       const on = stub()
       const createWatcher: typeof watch = stub().returns({ on, close: () => {} })
-      const fileSource = new FileSource(createWatcher)
+      const fileSource = new FileSource(createWatcher, readFile)
       const observer = fileSource.observe('cwd')
       const first = firstValueFrom(observer)
       on.getCall(2).args[1]('path')
-      expect(await first).to.deep.equal({ key: 'cwd/path', kind: 'update', hint: 'add' })
+      expect(await first).to.containSubset({ key: 'cwd/path', kind: 'update', hint: 'add' })
     })
     it('should react to unlink event', async () => {
       const on = stub()
       const createWatcher: typeof watch = stub().returns({ on, close: () => {} })
-      const fileSource = new FileSource(createWatcher)
+      const fileSource = new FileSource(createWatcher, readFile)
       const observer = fileSource.observe('cwd')
       const first = firstValueFrom(observer)
       on.getCall(3).args[1]('path')
@@ -56,7 +58,7 @@ describe('adbs', () => {
     it('should react to unlinkDir event', async () => {
       const on = stub()
       const createWatcher: typeof watch = stub().returns({ on, close: () => {} })
-      const fileSource = new FileSource(createWatcher)
+      const fileSource = new FileSource(createWatcher, readFile)
       const observer = fileSource.observe('cwd')
       const first = firstValueFrom(observer)
       on.getCall(4).args[1]('path')
@@ -66,7 +68,7 @@ describe('adbs', () => {
       const on = stub()
       const close = stub()
       const createWatcher: typeof watch = stub().returns({ on, close })
-      const fileSource = new FileSource(createWatcher)
+      const fileSource = new FileSource(createWatcher, readFile)
       const observer = fileSource.observe('cwd')
       const first = firstValueFrom(observer)
       on.getCall(5).args[1]('error')
