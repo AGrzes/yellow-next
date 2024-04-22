423ba42b83dd4b5bc7a465d0ab32bbdad9ce3d47
Grzegorz Abramczyk <abramczykg@gmail.com>
2024-04-22 22:38:57 +0200

    Inline state component

---
+++ extensions/state/src/components/InlineState.tsx
@@ -0,0 +1,38 @@
+import Badge from '@mui/material/Badge'
+import Chip from '@mui/material/Chip'
+import Dialog from '@mui/material/Dialog'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableRow from '@mui/material/TableRow'
+import lodash from 'lodash'
+import React, { useState } from 'react'
+const { last } = lodash
+
+export function InlineState({ state }: { state: any[] }) {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const currentState = last(state)
+  return (
+    <>
+      <Badge badgeContent={state.length} color="primary" showZero>
+        <Chip size="small" variant="outlined" label={currentState.state} onClick={() => setShowDetails(true)} />
+      </Badge>
+      {showDetails && (
+        <Dialog open={showDetails} onClose={() => setShowDetails(false)}>
+          <Table>
+            <TableBody>
+              {state.map((s, key) => (
+                <TableRow key={key}>
+                  <TableCell>{s.state}</TableCell>
+                  <TableCell>{s.date}</TableCell>
+                  <TableCell>{s.comment}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Dialog>
+      )}
+    </>
+  )
+}
