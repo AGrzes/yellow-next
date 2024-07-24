a7c857a380811bfe5cde3cddac68d5cc0df19854
Grzegorz Abramczyk <abramczykg@gmail.com>
2024-07-24 23:21:26 +0200

    Avoid needlesly initializing express

--- core/src/server/module.ts
+++ core/src/server/module.ts
@@ -3,6 +3,6 @@ import { ContainerModule } from 'inversify'
 import { HttpServer } from './server.js'
 
 export const serverModule = new ContainerModule((bind) => {
-  bind(express).toConstantValue(express())
+  bind(express).toDynamicValue(() => express())
   bind(HttpServer).toSelf().inSingletonScope()
 })
