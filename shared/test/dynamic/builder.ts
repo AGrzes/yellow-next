f60428ee13d3590950e1194e2119a8719e634c99
Grzegorz Abramczyk <abramczykg@gmail.com>
2024-09-19 22:13:47 +0200

    Default shcema context

--- shared/test/dynamic/builder.ts
+++ shared/test/dynamic/builder.ts
@@ -6,8 +6,52 @@ describe.only('dynamic', () => {
   describe('builder', () => {
     it('should build schema', () => {
       const s = schema()
-      const options = s.build()
-      expect(options).to.have.property('graph')
+      const g = s.build()
+      expect(g).to.have.property('graph')
+    })
+    it('should provide context', () => {
+      const s = schema()
+      const g = s.build()
+      expect(g)
+        .to.have.nested.property('graph.@context')
+        .containSubset({
+          '@context': {
+            yd: 'agrzes:yellow-next:dynamic:',
+            rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
+            label: 'http://www.w3.org/2000/01/rdf-schema#label',
+            root: 'agrzes:yellow-next:dynamic:root',
+            'rdfs:range': {
+              '@type': '@id',
+            },
+            properties: {
+              '@reverse': 'rdfs:domain',
+            },
+            id_pattern: {
+              '@id': 'yd:Class:id_pattern',
+            },
+            default_property: {
+              '@id': 'yd:Class:default_property',
+            },
+            internal: {
+              '@id': 'yd:Class:internal',
+            },
+            Property: {
+              '@id': 'yd:Property',
+              '@context': {
+                range: {
+                  '@id': 'rdfs:range',
+                  '@type': '@id',
+                },
+                name: 'yd:Property:name',
+                reverse_name: 'yd:Property:reverse_name',
+                predicate: 'yd:Property:predicate',
+                multiplicity: 'yd:Property:multiplicity',
+                reverseMultiplicity: 'yd:Property:reverse_multiplicity',
+                orderBy: 'yd:Property:order_by',
+              },
+            },
+          },
+        })
     })
   })
 })
