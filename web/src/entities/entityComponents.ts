83582aee4adbd5ec3dd330943534dfb539c3f593
Grzegorz Abramczyk <abramczykg@gmail.com>
2024-09-29 21:54:44 +0200

    Resolving multiple components

--- web/src/entities/entityComponents.ts
+++ web/src/entities/entityComponents.ts
@@ -2,12 +2,21 @@ import { ComponentType } from 'react'
 import { config } from '../config/index'
 
 export interface ResolveParams {
-  className: string
+  className: string | string[]
   kind: string
 }
 
 const componentMap = (await config<{ componentMap: Record<string, ComponentType<any>> }>()).componentMap || {}
 
 export function resolveComponent<P = {}>({ className, kind }: ResolveParams): ComponentType<P> {
-  return componentMap[`entity:${className}:${kind}`] || ((() => null) as ComponentType<P>)
+  if (!Array.isArray(className)) {
+    className = [className]
+  }
+  for (const clazz of className) {
+    const component = componentMap[`entity:${clazz}:${kind}`]
+    if (component) {
+      return component as ComponentType<P>
+    }
+  }
+  return (() => null) as ComponentType<P>
 }
