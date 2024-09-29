07c6e28b329758d55ec5fbd95148feeafef06820
Grzegorz Abramczyk <abramczykg@gmail.com>
2024-09-29 21:54:12 +0200

    Extracted config

--- web/src/entities/EntityCollection.tsx
+++ web/src/entities/EntityCollection.tsx
@@ -1,22 +1,8 @@
 import { SemanticProxy } from '@agrzes/yellow-next-shared/dynamic/access'
 import React from 'react'
-import { config } from '../config/index'
 import { EntityList } from './EntityList'
 import { EntityTree } from './EntityTree'
-
-export interface ClassConfig<T extends SemanticProxy> {
-  defaultCollectionDisplay?: string
-  treeOptions?: {
-    children: (parent: T) => T[]
-    parent: (child: T) => T
-  }
-}
-
-const classConfig = (await config<{ classConfig: Record<string, ClassConfig<any>> }>()).classConfig || {}
-
-export function resolveConfig<T extends SemanticProxy>(className: string): ClassConfig<T> {
-  return classConfig[className] || {}
-}
+import { resolveConfig } from './config'
 
 export function EntityCollection<T extends SemanticProxy>({ className }: { className: string }) {
   const config = resolveConfig<T>(className)
