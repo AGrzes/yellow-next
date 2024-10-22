a6ff88ddf50825a3109a0ad7c06de649628b270d
Grzegorz Abramczyk <abramczykg@gmail.com>
2024-10-22 10:40:08 +0200

    Generic component factory

--- web/src/entities/entityComponents.ts
+++ web/src/entities/entityComponents.ts
@@ -13,8 +13,17 @@ export interface ResolveParams {
   className: string | string[]
   kind: string
 }
+export type EntityComponentFactory = (className: string, kind: string) => ComponentType<any>
 
 const componentMap = (await config<{ componentMap: Record<string, ComponentType<any>> }>()).componentMap || {}
+const legacyComponentFactory: EntityComponentFactory = (className, kind) => componentMap[`entity:${className}:${kind}`]
+
+const directComponentFactory: EntityComponentFactory = (className, kind) => {
+  const config = resolveConfig(className)
+  return config.components?.[kind]
+}
+
+const componentFactories = [directComponentFactory, legacyComponentFactory]
 
 export function resolveComponent<P = {}>({ className, kind }: ResolveParams): ComponentType<P> {
   if (!Array.isArray(className)) {
@@ -22,7 +31,7 @@ export function resolveComponent<P = {}>({ className, kind }: ResolveParams): Co
   }
   for (const clazz of className) {
     const config = resolveConfig(clazz)
-    const component = config.components?.[kind] || componentMap[`entity:${clazz}:${kind}`]
+    const component = componentFactories.reduce((component, factory) => component || factory(clazz, kind), null)
     if (component) {
       return component as ComponentType<P>
     }
