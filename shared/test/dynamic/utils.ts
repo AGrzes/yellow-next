82992dd57ec9a5384e32034dce1c9aeeec12a4a7
Grzegorz Abramczyk <abramczykg@gmail.com>
2024-10-23 10:04:36 +0200

    Most specific class - more tests

--- shared/test/dynamic/utils.ts
+++ shared/test/dynamic/utils.ts
@@ -3,6 +3,8 @@ import 'mocha'
 import { mostSpecificClass } from '../../src/dynamic/utils.js'
 
 const A = { name: 'A', iri: 'A', properties: [] }
+const B = { name: 'B', iri: 'B', properties: [] }
+const C = { name: 'C', iri: 'C', properties: [], ancestors: [A] }
 
 describe('dynamic', () => {
   describe('accass', () => {
@@ -10,6 +12,9 @@ describe('dynamic', () => {
       describe('mostSpecificClass', () => {
         it('should return only class accepted', () => {
           expect(mostSpecificClass(A)).to.equal(A)
+        })
+        it('should return first of unrelated classes', () => {
+          expect(mostSpecificClass(A, B)).to.equal(A)
         })
       })
     })
