e9d8a079dfb94955d17b4b18b4ab4bbc27e99c0b
Grzegorz Abramczyk <abramczykg@gmail.com>
2024-10-25 18:49:32 +0200

    Relation list

--- web/src/entities/EntityList.tsx
+++ web/src/entities/EntityList.tsx
@@ -1,5 +1,7 @@
-import { List } from '@mui/material'
+import { List, ListSubheader } from '@mui/material'
+import { camelCase, upperFirst } from 'lodash'
 import React from 'react'
+import { EntityComponentType } from '../components/index.js'
 import { useModel } from '../model/index'
 import { useComponent } from './entityComponents'
 
@@ -15,4 +17,25 @@ export function EntityList({ className }: { className?: string }) {
       ))}
     </List>
   )
+}
+
+export function relationList(className: string, property: string, label?: string): EntityComponentType {
+  label = label || upperFirst(camelCase(property))
+
+  return ({ entity }) => {
+    const EntityListItem = useComponent(className, 'listItem')
+    let entities = entity[property]
+    if (!Array.isArray(entities)) {
+      entities = [entities]
+    }
+    return (
+      !!entities.length && (
+        <List subheader={<ListSubheader>{label}</ListSubheader>}>
+          {entities.map((entity) => (
+            <EntityListItem key={entity.iri} entity={entity} />
+          ))}
+        </List>
+      )
+    )
+  }
 }
