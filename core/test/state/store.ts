039f87beaea1262d2c1cf84e9ce4cac1853b209e
Grzegorz Abramczyk <abramczykg@gmail.com>
2024-11-13 10:14:23 +0100

    organizing tests

--- core/test/state/store.ts
+++ core/test/state/store.ts
@@ -7,51 +7,54 @@ import sinonChai from 'sinon-chai'
 import { FileStore } from '../../src/state/store'
 
 chai.use(sinonChai)
+describe('state', () => {
+  describe('store', () => {
+    describe('FileStore', () => {
+      const baseDir = '/tmp/store'
+      let store: FileStore<any>
+      let fsMock: any
 
-describe('FileStore', () => {
-  const baseDir = '/tmp/store'
-  let store: FileStore<any>
-  let fsMock: any
+      beforeEach(() => {
+        fsMock = {
+          readFile: sinon.stub(),
+          writeFile: sinon.stub(),
+          mkdir: sinon.stub(),
+        }
+        store = new FileStore(baseDir, fsMock)
+      })
 
-  beforeEach(() => {
-    fsMock = {
-      readFile: sinon.stub(),
-      writeFile: sinon.stub(),
-      mkdir: sinon.stub(),
-    }
-    store = new FileStore(baseDir, fsMock)
-  })
+      afterEach(() => {
+        sinon.restore()
+      })
 
-  afterEach(() => {
-    sinon.restore()
-  })
+      describe('get', () => {
+        it('should read and parse YAML file', async () => {
+          const key = ['test']
+          const filePath = path.join(baseDir, ...key) + '.yaml'
+          const fileContent = yaml.dump({ foo: 'bar' })
+          fsMock.readFile.resolves(fileContent)
 
-  describe('get', () => {
-    it('should read and parse YAML file', async () => {
-      const key = ['test']
-      const filePath = path.join(baseDir, ...key) + '.yaml'
-      const fileContent = yaml.dump({ foo: 'bar' })
-      fsMock.readFile.resolves(fileContent)
+          const result = await store.get(key)
 
-      const result = await store.get(key)
+          expect(fsMock.readFile).to.have.been.calledWith(filePath, 'utf8')
+          expect(result).to.deep.equal({ foo: 'bar' })
+        })
+      })
 
-      expect(fsMock.readFile).to.have.been.calledWith(filePath, 'utf8')
-      expect(result).to.deep.equal({ foo: 'bar' })
-    })
-  })
+      describe('put', () => {
+        it('should write YAML file and create directories if needed', async () => {
+          const key = ['test']
+          const value = { foo: 'bar' }
+          const filePath = path.join(baseDir, ...key) + '.yaml'
+          const dirPath = path.dirname(filePath)
+          const fileContent = yaml.dump(value)
 
-  describe('put', () => {
-    it('should write YAML file and create directories if needed', async () => {
-      const key = ['test']
-      const value = { foo: 'bar' }
-      const filePath = path.join(baseDir, ...key) + '.yaml'
-      const dirPath = path.dirname(filePath)
-      const fileContent = yaml.dump(value)
+          await store.put(key, value)
 
-      await store.put(key, value)
-
-      expect(fsMock.mkdir).to.have.been.calledWith(dirPath, { recursive: true })
-      expect(fsMock.writeFile).to.have.been.calledWith(filePath, fileContent, 'utf8')
+          expect(fsMock.mkdir).to.have.been.calledWith(dirPath, { recursive: true })
+          expect(fsMock.writeFile).to.have.been.calledWith(filePath, fileContent, 'utf8')
+        })
+      })
     })
   })
 })
