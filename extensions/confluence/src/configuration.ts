5ee73afdfda73fbf8c2d7cbc4ddd4b4205d56bf4
Grzegorz Abramczyk <abramczykg@gmail.com>
2024-11-19 22:13:33 +0100

    More configuration

--- extensions/confluence/src/configuration.ts
+++ extensions/confluence/src/configuration.ts
@@ -9,10 +9,12 @@ export interface ConfluenceConfiguration {
 export function configurationFromEnv(instance: string = null): ConfluenceConfiguration {
   const prefix = filter(['CONFLUENCE', instance]).join('_').toUpperCase()
   const site = process.env[`${prefix}_SITE`]
-  const email = process.env[`${prefix}_EMAIL`]
+  const username = process.env[`${prefix}_USERNAME`] || process.env[`${prefix}_EMAIL`]
   const token = process.env[`${prefix}_API_TOKEN`]
+  const auth = process.env[`${prefix}_AUTHORIZATION`] || 'basic'
   return {
     baseUrl: site,
-    authorization: `Basic ${Buffer.from(`${email}:${token}`).toString('base64')}`,
+    authorization:
+      auth === 'basic' ? `Basic ${Buffer.from(`${username}:${token}`).toString('base64')}` : `Bearer ${token}`,
   }
 }
--- extensions/confluence/test/configuration.ts
+++ extensions/confluence/test/configuration.ts
@@ -26,5 +26,25 @@ describe('configuration', () => {
         `Basic ${Buffer.from('test-email:test-token').toString('base64')}`
       )
     })
+    it('should return configuration with username and basic authorization', () => {
+      process.env.CONFLUENCE_SITE = 'https://test-site.com'
+      process.env.CONFLUENCE_USERNAME = 'test-username'
+      process.env.CONFLUENCE_API_TOKEN = 'test-token'
+      const configuration = configurationFromEnv()
+      expect(configuration).to.have.property('baseUrl', 'https://test-site.com')
+      expect(configuration).to.have.property(
+        'authorization',
+        `Basic ${Buffer.from('test-username:test-token').toString('base64')}`
+      )
+    })
+    it('should return configuration with bearer authorization', () => {
+      process.env.CONFLUENCE_SITE = 'https://test-site.com'
+      process.env.CONFLUENCE_EMAIL = 'test-email'
+      process.env.CONFLUENCE_API_TOKEN = 'test-token'
+      process.env.CONFLUENCE_AUTHORIZATION = 'bearer'
+      const configuration = configurationFromEnv()
+      expect(configuration).to.have.property('baseUrl', 'https://test-site.com')
+      expect(configuration).to.have.property('authorization', 'Bearer test-token')
+    })
   })
 })
