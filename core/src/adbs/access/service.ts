611cacdf6d823a053c0570c0c8cfdd730d652d92
Grzegorz Abramczyk <abramczykg@gmail.com>
2024-11-27 22:54:15 +0100

    Handling files and folders

--- core/src/adbs/access/service.ts
+++ core/src/adbs/access/service.ts
@@ -1,25 +1,40 @@
 import fsp from 'fs/promises'
 import { join } from 'path'
 
-export interface DocumentEntry {
+export interface Entry {
   name: string
   path: string
+  isFolder: boolean
+}
+
+export interface DocumentEntry extends Entry {
   formats: string[]
+  isFolder: false
+}
+
+export interface FolderEntry extends Entry {
+  isFolder: true
 }
 
 export class AccessService {
   constructor(
     private documentDirectory: string = 'documents',
-    private fs: Pick<typeof fsp, 'readdir'> = fsp
+    private fs: Pick<typeof fsp, 'readdir' | 'stat'> = fsp
   ) {}
 
-  public async listFiles(subPath: string = ''): Promise<DocumentEntry[]> {
+  public async listFiles(subPath: string = ''): Promise<Entry[]> {
     const directory = join(this.documentDirectory, subPath)
     const files = await this.fs.readdir(directory)
-    return files.map((file) => ({
-      name: file,
-      path: join(subPath, file),
-      formats: [],
-    }))
+    return Promise.all(
+      files.map(async (file) => {
+        const filePath = join(subPath, file)
+        const stats = await this.fs.stat(join(this.documentDirectory, filePath))
+        return {
+          name: file,
+          path: filePath,
+          ...(stats.isDirectory() ? { isFolder: true } : { formats: [], isFolder: false }),
+        }
+      })
+    )
   }
 }
--- core/test/adbs/access/service.ts
+++ core/test/adbs/access/service.ts
@@ -11,21 +11,40 @@ describe('adbs', () => {
       it('should list files in the directory', async () => {
         const service = new AccessService('test-documents', {
           readdir: async () => ['file1.txt', 'file2.txt'],
+          stat: async (path: string) => ({
+            isDirectory: () => false,
+          }),
         } as any)
         const files = await service.listFiles()
         expect(files).to.deep.equal([
-          { name: 'file1.txt', path: 'file1.txt', formats: [] },
-          { name: 'file2.txt', path: 'file2.txt', formats: [] },
+          { name: 'file1.txt', path: 'file1.txt', formats: [], isFolder: false },
+          { name: 'file2.txt', path: 'file2.txt', formats: [], isFolder: false },
         ])
       })
       it('should list files in the subdirectory', async () => {
         const service = new AccessService('test-documents', {
           readdir: async () => ['file1.txt', 'file2.txt'],
+          stat: async (path: string) => ({
+            isDirectory: () => false,
+          }),
         } as any)
         const files = await service.listFiles('subpath')
         expect(files).to.deep.equal([
-          { name: 'file1.txt', path: 'subpath/file1.txt', formats: [] },
-          { name: 'file2.txt', path: 'subpath/file2.txt', formats: [] },
+          { name: 'file1.txt', path: 'subpath/file1.txt', formats: [], isFolder: false },
+          { name: 'file2.txt', path: 'subpath/file2.txt', formats: [], isFolder: false },
+        ])
+      })
+      it('should list folders in the directory', async () => {
+        const service = new AccessService('test-documents', {
+          readdir: async () => ['folder1', 'folder2'],
+          stat: async (path: string) => ({
+            isDirectory: () => true,
+          }),
+        } as any)
+        const files = await service.listFiles()
+        expect(files).to.deep.equal([
+          { name: 'folder1', path: 'folder1', isFolder: true },
+          { name: 'folder2', path: 'folder2', isFolder: true },
         ])
       })
     })
