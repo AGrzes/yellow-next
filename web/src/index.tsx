5739b6bbd728de5d2fc980c58e55dff3817f1b9a
Grzegorz Abramczyk <abramczykg@gmail.com>
2024-12-15 23:17:33 +0100

    Styles for headers nesting

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import 'reflect-metadata'
 import { App } from './app'
-
+import './main.scss'
 
 const appElement = document.getElementById('root')
 createRoot(appElement).render(<App />)
