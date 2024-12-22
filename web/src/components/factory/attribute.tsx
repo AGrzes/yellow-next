83a32e1dc1c768c37d0833339defb942c85068d0
Grzegorz Abramczyk <abramczykg@gmail.com>
2024-12-22 22:34:04 +0100

    Basic attribute table

---
+++ web/src/components/factory/attribute.tsx
@@ -0,0 +1,59 @@
+import { Table, TableCell, TableRow } from '@mui/material'
+import lodash from 'lodash'
+import React, { useMemo } from 'react'
+import { EntityComponentType } from '../index.js'
+const { groupBy, mapValues, upperFirst, camelCase, map } = lodash
+
+export interface AttributeConfig {
+  labelProperty: string
+  valueProperty: string
+  groupProperty?: string
+}
+
+export function attributeTable(property: string, config: AttributeConfig): EntityComponentType {
+  const AttributeRow = ({ attribute }) => {
+    return (
+      <TableRow>
+        <TableCell>{attribute[config.labelProperty]}</TableCell>
+        <TableCell>{attribute[config.valueProperty]}</TableCell>
+      </TableRow>
+    )
+  }
+
+  if (config.groupProperty) {
+    return ({ entity, sx }) => {
+      const groups = useMemo(
+        () => groupBy(entity[property], (value) => value[config.groupProperty] || 'default'),
+        [entity]
+      )
+      const groupLabels = mapValues(groups, (values, key) => upperFirst(camelCase(key)))
+      const groupKeys = Object.keys(groups)
+      return (
+        <Table>
+          {groupKeys.map((key) => (
+            <>
+              {groups[key].length > 0 && (
+                <TableRow>
+                  <TableCell colSpan={2}>{groupLabels[key]}</TableCell>
+                </TableRow>
+              )}
+              {groups[key].map((attribute, key) => (
+                <AttributeRow key={key} attribute={attribute} />
+              ))}
+            </>
+          ))}
+        </Table>
+      )
+    }
+  } else {
+    return ({ entity }) => {
+      return (
+        <Table>
+          {entity[property].map((attribute, key) => (
+            <AttributeRow key={key} attribute={attribute} />
+          ))}
+        </Table>
+      )
+    }
+  }
+}
