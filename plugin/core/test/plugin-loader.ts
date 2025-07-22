9f3259f47208d58e333355258f4529f68ebec8e1
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-07-22 23:35:49 +0200

    Removed plan for tests

--- plugin/core/test/plugin-loader.ts
+++ plugin/core/test/plugin-loader.ts
@@ -9,13 +9,6 @@ const { expect } = chai.use(sinonChai).use(chaiAsPromised)
 describe('plugin', () => {
   describe('core', () => {
     describe('plugin-loader', () => {
-      /*
-        should reject invalid manifest version
-        should load plugin with default export function
-        should load plugin with whole module as function
-        should throw error if entrypoint does not export a function
-        should throw error if module fails to load
-      */
       it('should reject invalid manifest version', async () => {
         const manifest = { manifestVersion: '2', base: 'path/to/plugin', entrypoint: 'index.js' }
         await expect(loadPlugin(manifest as any)).to.be.rejectedWith('Unsupported plugin manifest version: 2')
