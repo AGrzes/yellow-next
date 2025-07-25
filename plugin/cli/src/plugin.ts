fc573dd6875fed9dae2783f5fae8d6190da0a99d
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-07-25 22:15:23 +0200

    Command plugin registration

--- plugin/cli/src/plugin.ts
+++ plugin/cli/src/plugin.ts
@@ -1,6 +1,13 @@
-function entrypoint() {
-  // Register root command
-  console.log('Plugin entrypoint called')
+import { PluginContext } from '@agrzes/yellow-next-plugin-core'
+import { Command, COMMAND, PROGRAM_NAME, ROOT_COMMAND } from './index.js'
+function entrypoint({ manifest, registry }: PluginContext): void {
+  registry.register({
+    identifier: COMMAND,
+    qualifier: ROOT_COMMAND,
+    factory: () => {
+      return new Command(PROGRAM_NAME).help()
+    },
+  })
 }
 
 export default entrypoint
