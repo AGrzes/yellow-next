2710721d176f7a21c60a444791099261bb9feed4
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-07-25 10:56:33 +0200

    Rename

--- plugin/core/test/index.ts
+++ plugin/core/test/index.ts
@@ -1,13 +1,13 @@
 import * as chai from 'chai'
 import 'mocha'
 import sinonChai from 'sinon-chai'
-import { lookupManifest } from '../src/index.js'
+import { lookupManifests } from '../src/index.js'
 const { expect } = chai.use(sinonChai)
 
 describe('plugin', () => {
   describe('core', () => {
     it('should export lookupManifest', () => {
-      expect(lookupManifest).to.be.a('function')
+      expect(lookupManifests).to.be.a('function')
     })
   })
 })
--- plugin/core/test/lookup-manifest.ts
+++ plugin/core/test/lookup-manifest.ts
@@ -2,7 +2,7 @@ import * as chai from 'chai'
 import 'mocha'
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
-import { makeLookupManifest } from '../src/lookup-manifests.js'
+import { makeLookupManifests } from '../src/lookup-manifests.js'
 const { expect } = chai.use(sinonChai)
 
 describe('plugin', () => {
@@ -12,7 +12,7 @@ describe('plugin', () => {
         const requirePaths = sinon.stub().returns(['path/to/plugins'])
         const glob = sinon.stub().resolves([])
         const readFile = sinon.stub()
-        const lookupManifest = makeLookupManifest(requirePaths, glob, readFile)
+        const lookupManifest = makeLookupManifests(requirePaths, glob, readFile)
         await lookupManifest()
         expect(requirePaths).to.have.been.calledOnceWith('*')
         expect(glob).to.have.been.calledOnceWith([
@@ -24,7 +24,7 @@ describe('plugin', () => {
         const requirePaths = sinon.stub().returns(['path/to/plugins'])
         const glob = sinon.stub().resolves(['path/to/plugins/plugin-a/yellow-plugin.json'])
         const readFile = sinon.stub().resolves(JSON.stringify({ name: 'plugin-a' }))
-        const lookupManifest = makeLookupManifest(requirePaths, glob, readFile)
+        const lookupManifest = makeLookupManifests(requirePaths, glob, readFile)
         const manifests = await lookupManifest()
         expect(manifests).to.have.lengthOf(1)
         expect(manifests[0]).to.deep.equal({ name: 'plugin-a', base: 'path/to/plugins/plugin-a' })
@@ -34,7 +34,7 @@ describe('plugin', () => {
         const requirePaths = sinon.stub().returns(['path/to/plugins'])
         const glob = sinon.stub().resolves(['path/to/plugins/plugin-a/yellow-plugin.json'])
         const readFile = sinon.stub().rejects(new Error('Failed to read file'))
-        const lookupManifest = makeLookupManifest(requirePaths, glob, readFile)
+        const lookupManifest = makeLookupManifests(requirePaths, glob, readFile)
         const manifests = await lookupManifest()
         expect(manifests).to.have.lengthOf(0)
         expect(readFile).to.have.been.calledOnceWith('path/to/plugins/plugin-a/yellow-plugin.json', 'utf-8')
