c169cd390da5429f58943e9e6887333c09cdfc67
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-07-25 22:13:07 +0200

    Plugin dir in env

--- plugin/core/src/lookup-manifests.ts
+++ plugin/core/src/lookup-manifests.ts
@@ -1,5 +1,4 @@
 import debug from 'debug'
-import { createRequire } from 'node:module'
 import { dirname, join } from 'node:path'
 import { PluginManifest } from './manifest.js'
 const log = debug('yellow:plugin:core:lookup-manifests')
@@ -10,10 +9,17 @@ export function makeLookupManifests(
   readFile: typeof import('node:fs/promises').readFile
 ) {
   return async function lookupManifests(): Promise<PluginManifest[]> {
-    const require = createRequire(import.meta.url)
+    const lookupPaths = requirePaths('*')
+    log('Looking up plugin manifests in paths:', lookupPaths)
+    if (process.env.YELLOW_PLUGIN_DIR) {
+      log('Using custom plugin directory from environment:', process.env.YELLOW_PLUGIN_DIR)
+      lookupPaths.push(...process.env.YELLOW_PLUGIN_DIR.split(','))
+    }
+    log('Final lookup paths:', lookupPaths)
     const paths = await glob(
-      requirePaths('*').flatMap((p) => [join(p, '*', 'yellow-plugin.json'), join(p, '@*', '*', 'yellow-plugin.json')])
+      lookupPaths.flatMap((p) => [join(p, '*', 'yellow-plugin.json'), join(p, '@*', '*', 'yellow-plugin.json')])
     )
+    log('Found plugin manifest paths:', paths)
     const manifests = await Promise.all(
       paths.map(async (path) => {
         try {
