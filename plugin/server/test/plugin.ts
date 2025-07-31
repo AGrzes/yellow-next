e708747160a90dd192aea678648f7be3de3aa223
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-07-31 22:58:46 +0200

    Basic test

---
+++ plugin/server/test/plugin.ts
@@ -0,0 +1,43 @@
+import {
+  ApplicationContext,
+  CONTEXT,
+  ServiceRegistration,
+  ServiceRequest,
+  ServiceSelector,
+} from '@agrzes/yellow-next-plugin-core'
+import * as chai from 'chai'
+import { Application } from 'express'
+import 'mocha'
+import sinonChai from 'sinon-chai'
+import { SERVER } from '../src/index.js'
+import plugin from '../src/plugin.js'
+const { expect } = chai.use(sinonChai)
+
+describe('plugin', () => {
+  describe('cli', () => {
+    describe('server', () => {
+      let registration: ServiceRegistration<Application, readonly [ServiceSelector<ApplicationContext>]>
+      before(() => {
+        plugin({
+          manifest: {
+            base: 'base',
+            manifestVersion: '1',
+          },
+          registry: {
+            register: (options) => {
+              if (options.identifier === SERVER) {
+                registration = options as unknown as ServiceRegistration<
+                  Application,
+                  readonly [ServiceSelector<ApplicationContext>]
+                >
+              }
+            },
+          },
+        })
+      })
+      it('should declare dependencies', () => {
+        expect(registration.dependencies).to.be.deep.equal([ServiceRequest.named(CONTEXT, null)])
+      })
+    })
+  })
+})
