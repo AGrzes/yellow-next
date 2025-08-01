d9bd0e007bf309594c9242fda83f6a0ea7e92c06
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-08-01 11:04:49 +0200

    Server registration test

--- plugin/server/test/plugin.ts
+++ plugin/server/test/plugin.ts
@@ -8,6 +8,7 @@ import {
 import * as chai from 'chai'
 import { Application } from 'express'
 import 'mocha'
+import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
 import { SERVER } from '../src/index.js'
 import plugin from '../src/plugin.js'
@@ -47,7 +48,14 @@ describe('plugin', () => {
       it('should not have qualifier', () => {
         expect(registration.qualifier).to.be.undefined
       })
-
+      it('should register a server', async () => {
+        expect(registration.factory).to.be.a('function')
+        const context = {
+          get: sinon.stub().resolves([]),
+        } as unknown as ApplicationContext
+        const app = await registration.factory([context])
+        expect(app).to.be.a('function')
+      })
     })
   })
 })
