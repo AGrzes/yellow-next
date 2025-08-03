b8708714e4595b0aa1bf50e92224b87b19629b26
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-08-03 20:44:01 +0200

    Refectored server test

--- plugin/server/test/plugin.ts
+++ plugin/server/test/plugin.ts
@@ -18,13 +18,13 @@ const { expect } = chai.use(sinonChai)
 function registrationTest<T, D extends readonly ServiceRequest<any>[]>(
   identifier: ServiceIdentifier<T>,
   options: {
-    factoryTests: (registrationSource: () => ServiceRegistration<T, readonly []>) => void
+    factoryTests: (registrationSource: () => ServiceRegistration<T, D>) => void
     dependencies?: D
     provided?: readonly ServiceIdentifier<T>[]
     qualifier?: string
   }
 ) {
-  let registration: ServiceRegistration<T, readonly []>
+  let registration: ServiceRegistration<T, D>
   before(() => {
     plugin({
       manifest: {
@@ -34,7 +34,7 @@ function registrationTest<T, D extends readonly ServiceRequest<any>[]>(
       registry: {
         register: (options) => {
           if ((options.identifier as unknown) === identifier) {
-            registration = options as unknown as ServiceRegistration<T, readonly []>
+            registration = options as unknown as ServiceRegistration<T, D>
           }
         },
       },
@@ -90,61 +90,37 @@ describe('plugin', () => {
       })
     })
     describe('server', () => {
-      let registration: ServiceRegistration<Application, readonly [ServiceSelector<ApplicationContext>, typeof EXPRESS]>
-      before(() => {
-        plugin({
-          manifest: {
-            base: 'base',
-            manifestVersion: '1',
-          },
-          registry: {
-            register: (options) => {
-              if (options.identifier === SERVER) {
-                registration = options as unknown as ServiceRegistration<
-                  Application,
-                  readonly [ServiceSelector<ApplicationContext>, typeof EXPRESS]
-                >
-              }
-            },
-          },
-        })
-      })
-      it('should register server', () => {
-        expect(registration).to.be.an('object')
-      })
-      it('should declare dependencies', () => {
-        expect(registration.dependencies).to.be.deep.equal([ServiceRequest.named(CONTEXT, null), EXPRESS])
-      })
-      it('should not have provided services', () => {
-        expect(registration.provided).to.be.undefined
-      })
-      it('should not have qualifier', () => {
-        expect(registration.qualifier).to.be.undefined
-      })
-      it('should register a server', async () => {
-        expect(registration.factory).to.be.a('function')
-        const context = {
-          get: sinon.stub().resolves([]),
-        } as unknown as ApplicationContext
-        const appMock = {
-          use: sinon.stub(),
-        }
-        const appFactory = sinon.stub().returns(appMock) as unknown as () => Express
-        const app = await registration.factory([context, appFactory])
-        expect(app).to.be.equals(appMock)
-      })
-      it('should register a routes with server', async () => {
-        expect(registration.factory).to.be.a('function')
-        const context = {
-          get: sinon.stub().resolves(['route']),
-        }
-        const appMock = {
-          use: sinon.stub(),
-        }
-        const appFactory = sinon.stub().returns(appMock) as unknown as () => Express
-        await registration.factory([context as unknown as ApplicationContext, appFactory])
-        expect(context.get).to.have.been.calledWith(ServiceRequest.multiple('server.router'))
-        expect(appMock.use).to.have.been.calledWith('route')
+      registrationTest<Application, readonly [ServiceSelector<ApplicationContext>, typeof EXPRESS]>(SERVER, {
+        dependencies: [ServiceRequest.named(CONTEXT, null), EXPRESS],
+        factoryTests: (registrationSource) => {
+          it('should register a server', async () => {
+            const registration = registrationSource()
+            expect(registration.factory).to.be.a('function')
+            const context = {
+              get: sinon.stub().resolves([]),
+            } as unknown as ApplicationContext
+            const appMock = {
+              use: sinon.stub(),
+            }
+            const appFactory = sinon.stub().returns(appMock) as unknown as () => Express
+            const app = await registration.factory([context, appFactory])
+            expect(app).to.be.equals(appMock)
+          })
+          it('should register a routes with server', async () => {
+            const registration = registrationSource()
+            expect(registration.factory).to.be.a('function')
+            const context = {
+              get: sinon.stub().resolves(['route']),
+            }
+            const appMock = {
+              use: sinon.stub(),
+            }
+            const appFactory = sinon.stub().returns(appMock) as unknown as () => Express
+            await registration.factory([context as unknown as ApplicationContext, appFactory])
+            expect(context.get).to.have.been.calledWith(ServiceRequest.multiple('server.router'))
+            expect(appMock.use).to.have.been.calledWith('route')
+          })
+        },
       })
     })
   })
