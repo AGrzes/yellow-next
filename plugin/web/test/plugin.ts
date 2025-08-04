e2d197b5a73887d7c156bc8be45e5325b8413b56
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-08-04 20:21:56 +0200

    Test for vite server factory

--- plugin/web/test/plugin.ts
+++ plugin/web/test/plugin.ts
@@ -4,12 +4,26 @@ import * as chai from 'chai'
 import 'mocha'
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
+import { createServer } from 'vite'
 import { VITE_ROUTER } from '../src/index.js'
 import plugin, { VITE_SERVER_FACTORY } from '../src/plugin.js'
 const { expect } = chai.use(sinonChai)
 
 describe('plugin', () => {
   describe('cli', () => {
+    describe('vite server factory', () => {
+      registrationTest<typeof createServer, readonly []>(plugin, VITE_SERVER_FACTORY, {
+        factoryTests: (registrationSource) => {
+          it('should register a vite server factory', async () => {
+            const registration = registrationSource()
+            expect(registration.factory).to.be.a('function')
+            const registered = await registration.factory([])
+            expect(registered).to.be.a('function')
+            expect(registered).to.be.equal(createServer)
+          })
+        },
+      })
+    })
     describe('vite router', () => {
       registrationTest<Router, readonly [typeof VITE_SERVER_FACTORY]>(plugin, VITE_ROUTER, {
         provided: [ROUTER],
