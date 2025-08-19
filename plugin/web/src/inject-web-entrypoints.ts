ce335aff61a35d0ca94a7a9dfdde437e243275ae
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-08-19 22:23:57 +0200

    Plugin for injecting entrypoints

---
+++ plugin/web/src/inject-web-entrypoints.ts
@@ -0,0 +1,24 @@
+import { HtmlTagDescriptor, Plugin } from 'vite'
+import { WebEntrypoint } from './index.js'
+
+export function injectWebEntrypoints(entrypoints: WebEntrypoint[]): Plugin {
+  return {
+    name: 'inject-web-entrypoints',
+    enforce: 'pre',
+    config(old) {
+      return {
+        server: { fs: { allow: [...(old?.server?.fs?.allow || []), ...entrypoints.map((e) => e.root)] } },
+      }
+    },
+    transformIndexHtml(html) {
+      const tags = entrypoints.map(
+        (e): HtmlTagDescriptor => ({
+          tag: 'script',
+          attrs: { type: 'module', src: `/@fs/${e.script.replace(/\\/g, '/')}` },
+          injectTo: 'body',
+        })
+      )
+      return { html, tags }
+    },
+  }
+}
