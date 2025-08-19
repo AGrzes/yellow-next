0d7947a9102652de4e7bcb72caab494be71fbc75
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-08-19 22:24:10 +0200

    Web entrypoint defintiion

--- plugin/web/src/index.ts
+++ plugin/web/src/index.ts
@@ -1,1 +1,10 @@
+import { ServiceIdentifier } from '@agrzes/yellow-next-plugin-core'
+
 export const VITE_ROUTER = 'web.vite-router'
+
+export interface WebEntrypoint {
+  script: string
+  root: string
+}
+
+export const WebEntrypoint: ServiceIdentifier<WebEntrypoint> = 'web.entrypoint'
