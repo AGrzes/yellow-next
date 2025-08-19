ede8d14444d9669b76451558dc7cef5de7f791f9
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-08-19 22:24:24 +0200

    Configured vite plugins

--- plugin/web/src/plugin.ts
+++ plugin/web/src/plugin.ts
@@ -1,8 +1,10 @@
-import { PluginContext, ServiceIdentifier } from '@agrzes/yellow-next-plugin-core'
+import { PluginContext, ServiceIdentifier, ServiceRequest } from '@agrzes/yellow-next-plugin-core'
 import { ROUTER, ROUTER_FACTORY } from '@agrzes/yellow-next-plugin-server'
 import { join } from 'path'
 import { createServer } from 'vite'
-import { VITE_ROUTER } from './index.js'
+import { VITE_ROUTER, WebEntrypoint } from './index.js'
+import { injectWebEntrypoints } from './inject-web-entrypoints.js'
+import { resolveBareFromImporter } from './resolve-bare-from-importer.js'
 
 export const VITE_SERVER_FACTORY: ServiceIdentifier<typeof createServer> = 'vite.server.factory'
 function entrypoint({ manifest, registry }: PluginContext): void {
@@ -14,10 +16,11 @@ function entrypoint({ manifest, registry }: PluginContext): void {
 
   registry.register({
     identifier: VITE_ROUTER,
-    dependencies: [VITE_SERVER_FACTORY, ROUTER_FACTORY],
-    factory: async ([viteServerFactory, routerFactory]) => {
+    dependencies: [VITE_SERVER_FACTORY, ROUTER_FACTORY, ServiceRequest.multiple(WebEntrypoint)],
+    factory: async ([viteServerFactory, routerFactory, entrypoints]) => {
       const router = routerFactory()
       const vite = await viteServerFactory({
+        plugins: [resolveBareFromImporter(), injectWebEntrypoints(entrypoints)],
         root: join(manifest.base, 'web'),
         server: {
           middlewareMode: true,
