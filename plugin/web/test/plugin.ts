ceb99858c888c03bfaab9900439e322e83f5624b
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-08-20 19:50:51 +0200

    Updated tests

--- plugin/web/test/plugin.ts
+++ plugin/web/test/plugin.ts
@@ -1,3 +1,4 @@
+import { MultipleServiceSelector, ServiceRequest } from '@agrzes/yellow-next-plugin-core'
 import { Router, ROUTER, ROUTER_FACTORY } from '@agrzes/yellow-next-plugin-server'
 import { registrationTest } from '@agrzes/yellow-next-plugin-test'
 import * as chai from 'chai'
@@ -5,7 +6,7 @@ import 'mocha'
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
 import { createServer } from 'vite'
-import { VITE_ROUTER } from '../src/index.js'
+import { VITE_ROUTER, WebEntrypoint } from '../src/index.js'
 import plugin, { VITE_SERVER_FACTORY } from '../src/plugin.js'
 const { expect } = chai.use(sinonChai)
 
@@ -25,9 +26,12 @@ describe('plugin', () => {
       })
     })
     describe('vite router', () => {
-      registrationTest<Router, readonly [typeof VITE_SERVER_FACTORY, typeof ROUTER_FACTORY]>(plugin, VITE_ROUTER, {
+      registrationTest<
+        Router,
+        readonly [typeof VITE_SERVER_FACTORY, typeof ROUTER_FACTORY, MultipleServiceSelector<WebEntrypoint>]
+      >(plugin, VITE_ROUTER, {
         provided: [ROUTER],
-        dependencies: [VITE_SERVER_FACTORY, ROUTER_FACTORY],
+        dependencies: [VITE_SERVER_FACTORY, ROUTER_FACTORY, ServiceRequest.multiple(WebEntrypoint)],
         factoryTests: (registrationSource) => {
           it('should register a vite server', async () => {
             const registration = registrationSource()
@@ -40,14 +44,16 @@ describe('plugin', () => {
               use: sinon.stub(),
             }
             const routerFactory = sinon.stub().returns(router) as unknown as () => Router
-            const registered = await registration.factory([viteServerFactory, routerFactory])
+            const registered = await registration.factory([viteServerFactory, routerFactory, []])
             expect(registered).to.be.equals(router)
-            expect(viteServerFactory).to.be.calledOnceWith({
-              root: 'base/web',
-              server: {
-                middlewareMode: true,
-              },
-            })
+            expect(viteServerFactory).to.be.calledOnceWith(
+              sinon.match({
+                root: 'base/web',
+                server: {
+                  middlewareMode: true,
+                },
+              })
+            )
             expect(routerFactory).to.be.calledOnce
             expect(router.use).to.be.calledOnceWith(middlewares)
           })
