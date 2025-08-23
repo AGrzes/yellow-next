d6a29253ff1daf3ed08213f07bb5e36fa3b3e8db
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-08-23 20:46:06 +0200

    Event list compontnt

--- plugin/event-log/web/components/last-events/index.tsx
+++ plugin/event-log/web/components/last-events/index.tsx
@@ -1,3 +1,17 @@
+import { Stack } from '@chakra-ui/react'
+import { useListEntries } from '../../query/index.js'
+
 export function LastEvents() {
-  return <div>Last Events</div>
+  const events = useListEntries()
+
+  return (
+    <Stack dir="column" gap={1}>
+      {events.data?.map((event) => (
+        <div key={event.id}>
+          <small>{event.createdAt}</small>
+          <span>{event.content}</span>
+        </div>
+      ))}
+    </Stack>
+  )
 }
