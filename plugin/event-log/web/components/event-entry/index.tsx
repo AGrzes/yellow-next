53b7a6a6bd7138b8dadc55596122b70f6119b02f
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-08-23 20:52:31 +0200

    Improved entry UX

--- plugin/event-log/web/components/event-entry/index.tsx
+++ plugin/event-log/web/components/event-entry/index.tsx
@@ -5,23 +5,29 @@ import { usePostEntry } from '../../query/index.js'
 export function EventEntry() {
   const postEntry = usePostEntry()
   const [content, setContent] = useState('')
+
+  const submit = () => {
+    postEntry.mutate(
+      { content },
+      {
+        onSuccess() {
+          setContent('')
+        },
+      }
+    )
+  }
   return (
     <div>
-      <Input onChange={(e) => setContent(e.target.value)} value={content} />
-      <Button
-        onClick={() =>
-          postEntry.mutate(
-            { content },
-            {
-              onSuccess() {
-                setContent('')
-              },
-            }
-          )
-        }
-      >
-        Add
-      </Button>
+      <Input
+        onChange={(e) => setContent(e.target.value)}
+        value={content}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            submit()
+          }
+        }}
+      />
+      <Button onClick={submit}>Add</Button>
     </div>
   )
 }
