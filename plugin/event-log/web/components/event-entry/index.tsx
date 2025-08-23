1a885a024a12db7a480f5101ea5d56efcd20ce19
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-08-23 20:46:17 +0200

    Event entry component

--- plugin/event-log/web/components/event-entry/index.tsx
+++ plugin/event-log/web/components/event-entry/index.tsx
@@ -1,3 +1,27 @@
+import { Button, Input } from '@chakra-ui/react'
+import { useState } from 'react'
+import { usePostEntry } from '../../query/index.js'
+
 export function EventEntry() {
-  return <div>Event Entry</div>
+  const postEntry = usePostEntry()
+  const [content, setContent] = useState('')
+  return (
+    <div>
+      <Input onChange={(e) => setContent(e.target.value)} value={content} />
+      <Button
+        onClick={() =>
+          postEntry.mutate(
+            { content },
+            {
+              onSuccess() {
+                setContent('')
+              },
+            }
+          )
+        }
+      >
+        Add
+      </Button>
+    </div>
+  )
 }
