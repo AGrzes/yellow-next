5ae3e0536b2dcecc963318d9fb7e78a9ec299acc
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-08-23 20:46:30 +0200

    Set up react query

--- plugin/event-log/web/app.tsx
+++ plugin/event-log/web/app.tsx
@@ -1,11 +1,17 @@
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from '@tanstack/react-router'
+import { useState } from 'react'
 import { router } from './router.js'
 
+
 export function App() {
+  const [queryClient] = useState(() => new QueryClient())
   return (
     <ChakraProvider value={defaultSystem}>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ChakraProvider>
   )
 }
