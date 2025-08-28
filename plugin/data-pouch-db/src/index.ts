f031c1e74f2e4c256e72c5bf2aac423d731fa8b0
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-08-28 21:08:29 +0200

    Additional plugin identifier

--- plugin/data-pouch-db/src/index.ts
+++ plugin/data-pouch-db/src/index.ts
@@ -1,5 +1,5 @@
 import { ServiceIdentifier } from '@agrzes/yellow-next-plugin-core'
-import { Catalog } from './catalog.js'
-
+import { Catalog, PouchDbConfig } from './catalog.js'
 
 export const POUCHDB_CATALOG: ServiceIdentifier<Catalog> = 'data-pouch-db.catalog'
+export const POUCHDB_CONFIG: ServiceIdentifier<PouchDbConfig> = 'data-pouch-db.config'
--- plugin/data-pouch-db/test/index.ts
+++ plugin/data-pouch-db/test/index.ts
@@ -1,13 +1,16 @@
 import * as chai from 'chai'
 import 'mocha'
 import sinonChai from 'sinon-chai'
-import { POUCHDB_CATALOG } from '../src/index.js'
+import { POUCHDB_CATALOG, POUCHDB_CONFIG } from '../src/index.js'
 const { expect } = chai.use(sinonChai)
 
 describe('plugin', () => {
   describe('data-pouch-db', () => {
     it('should export POUCHDB_CATALOG', () => {
       expect(POUCHDB_CATALOG).to.be.a('string')
+    })
+    it('should export POUCHDB_CONFIG', () => {
+      expect(POUCHDB_CONFIG).to.be.a('string')
     })
   })
 })
