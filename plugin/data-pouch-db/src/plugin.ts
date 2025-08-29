ddc2f5c306d8029b5656b77a39aaef91a556224b
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-08-29 10:38:59 +0200

    Moved helper function

--- plugin/data-pouch-db/src/plugin.ts
+++ plugin/data-pouch-db/src/plugin.ts
@@ -1,17 +1,14 @@
-import { PluginContext, ServiceIdentifier, ServiceRequest } from '@agrzes/yellow-next-plugin-core'
+import {
+  factoryFromConstructor,
+  PluginContext,
+  ServiceIdentifier,
+  ServiceRequest,
+} from '@agrzes/yellow-next-plugin-core'
 import PouchDB from 'pouchdb-node'
 import { PouchDBFactory, StaticCatalog } from './catalog.js'
 import { POUCHDB_CATALOG, POUCHDB_CONFIG } from './index.js'
 
 export const POUCHDB_FACTORY: ServiceIdentifier<PouchDBFactory> = 'data-pouch-db.factory'
-
-export function factoryFromConstructor<C extends new (...args: any) => any>(Ctor: C) {
-  type A = ConstructorParameters<C>
-  type I = InstanceType<C>
-  return () =>
-    (...args: A): I =>
-      new Ctor(...args)
-}
 
 function entrypoint({ manifest, registry }: PluginContext): void {
   registry.register({
