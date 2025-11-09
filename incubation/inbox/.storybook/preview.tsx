39f5c9f06d1b616dc573a846f6a2106c52666f34
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-11-09 21:01:33 +0100

    Preview config

---
+++ incubation/inbox/.storybook/preview.tsx
@@ -0,0 +1,21 @@
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import type { Preview } from '@storybook/react-vite'
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <ChakraProvider value={defaultSystem}>
+        <Story />
+      </ChakraProvider>
+    ),
+  ],
+}
+
+export default preview
