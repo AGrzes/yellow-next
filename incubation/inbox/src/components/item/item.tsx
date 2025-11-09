df04fcc4eaf0973bc23314208285979f531eac6c
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-11-09 20:32:29 +0100

    Labels in title line

--- incubation/inbox/src/components/item/item.tsx
+++ incubation/inbox/src/components/item/item.tsx
@@ -1,4 +1,4 @@
-import { Box, type BoxProps, Button, ButtonGroup, Flex, Text, Wrap } from '@chakra-ui/react'
+import { Box, Button, ButtonGroup, Flex, Text, Wrap } from '@chakra-ui/react'
 import type { Content, Item } from '@model/item.ts'
 
 function ContentDisplay({ content, ...boxProps }: { content: Content } & BoxProps) {
@@ -24,28 +24,30 @@ export function ItemLine({ item }: { item: Item }) {
       justifyContent="space-between"
     >
       <Flex direction="column" flex="1">
-        <Text fontSize="lg" fontWeight={item.read ? 'normal' : 'bold'}>
-          {item.title}
-        </Text>
+        <Flex alignItems="center" gap="2">
+          <Text fontSize="lg" fontWeight={item.read ? 'normal' : 'bold'}>
+            {item.title}
+          </Text>
+          {item.labels && (
+            <Wrap>
+              {Object.entries(item.labels).map(([key, value]) => (
+                <Box
+                  key={key}
+                  as="span"
+                  backgroundColor="blue.100"
+                  color="blue.800"
+                  paddingX="2"
+                  paddingY="1"
+                  borderRadius="md"
+                  fontSize="sm"
+                >
+                  {key}: {value}
+                </Box>
+              ))}
+            </Wrap>
+          )}
+        </Flex>
         {item.summary && <ContentDisplay content={item.summary} fontSize="sm" color="gray.500" />}
-        {item.labels && (
-          <Wrap mt="2">
-            {Object.entries(item.labels).map(([key, value]) => (
-              <Box
-                key={key}
-                as="span"
-                backgroundColor="blue.100"
-                color="blue.800"
-                paddingX="2"
-                paddingY="1"
-                borderRadius="md"
-                fontSize="sm"
-              >
-                {key}: {value}
-              </Box>
-            ))}
-          </Wrap>
-        )}
       </Flex>
       <ButtonGroup gap="2" position="absolute" right="4" top="50%" transform="translateY(-50%)">
         <Button variant="outline" colorScheme="green">
