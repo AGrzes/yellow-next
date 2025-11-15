82d40bbd70265a999c57b1e640ca5607586a6c60
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-11-15 21:05:39 +0100

    Entrypoint

--- incubation/inbox/src/main.tsx
+++ incubation/inbox/src/main.tsx
@@ -1,10 +1,19 @@
+import { createTheme, MantineProvider } from '@mantine/core'
+import '@mantine/core/styles.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router'
 import './index.css'
-import App from './App.tsx'
+import { router } from './routes/index.tsx'
+import { createInboxService, InboxServiceProvider } from './service/inbox/index.ts'
+export const theme = createTheme({})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <InboxServiceProvider value={createInboxService()}>
+        <RouterProvider router={router} />
+      </InboxServiceProvider>
+    </MantineProvider>
   </StrictMode>
 )
