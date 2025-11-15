226695282b2a1fe24b1dae3273b9d9fff1c03907
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-11-15 21:40:50 +0100

    Remove outer margins

--- incubation/inbox/src/components/content-display/content-display.tsx
+++ incubation/inbox/src/components/content-display/content-display.tsx
@@ -2,10 +2,13 @@ import type { BoxProps } from '@mantine/core'
 import { Box } from '@mantine/core'
 import type { Content } from '@model/item.ts'
 import { marked } from 'marked'
+import styles from './content-display.module.css'
 
 export function ContentDisplay({ content, ...boxProps }: { content: Content } & BoxProps) {
   if ('html' in content) {
-    return <Box {...boxProps} dangerouslySetInnerHTML={{ __html: content.html }} />
+    return <Box {...boxProps} dangerouslySetInnerHTML={{ __html: content.html }} className={styles.block} />
   }
-  return <Box {...boxProps} dangerouslySetInnerHTML={{ __html: marked.parse(content.markdown) }} />
+  return (
+    <Box {...boxProps} dangerouslySetInnerHTML={{ __html: marked.parse(content.markdown) }} className={styles.block} />
+  )
 }
