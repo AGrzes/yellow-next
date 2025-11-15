195e572b9aa0fb80b64941b46a09c16a286a2379
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-11-15 21:42:28 +0100

    Reduced labels

--- incubation/inbox/src/components/label-display/label-display.tsx
+++ incubation/inbox/src/components/label-display/label-display.tsx
@@ -2,7 +2,7 @@ import { Badge } from '@mantine/core'
 
 export function LabelDisplay({ labelKey, labelValue }: { labelKey: string; labelValue: string }) {
   return (
-    <Badge variant="light" color="blue">
+    <Badge variant="light" color="blue" size="xs">
       {labelKey}: {labelValue}
     </Badge>
   )
