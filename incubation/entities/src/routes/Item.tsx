99ed00cbb93757159fa507448636512312630e1b
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-12-07 22:26:11 +0100

    Removed dead code

--- incubation/entities/src/routes/Item.tsx
+++ incubation/entities/src/routes/Item.tsx
@@ -3,56 +3,6 @@ import { vanillaCells, vanillaRenderers } from '@jsonforms/vanilla-renderers'
 import { useState } from 'react'
 import { useLoaderData, type LoaderFunction } from 'react-router'
 
-const schema = {
-  type: 'object',
-  properties: {
-    name: {
-      type: 'string',
-      minLength: 1,
-    },
-    done: {
-      type: 'boolean',
-    },
-    due_date: {
-      type: 'string',
-      format: 'date',
-    },
-    recurrence: {
-      type: 'string',
-      enum: ['Never', 'Daily', 'Weekly', 'Monthly'],
-    },
-  },
-  required: ['name', 'due_date'],
-}
-const uischema = {
-  type: 'VerticalLayout',
-  elements: [
-    {
-      type: 'Control',
-      label: false,
-      scope: '#/properties/done',
-    },
-    {
-      type: 'Control',
-      scope: '#/properties/name',
-    },
-    {
-      type: 'HorizontalLayout',
-      elements: [
-        {
-          type: 'Control',
-          scope: '#/properties/due_date',
-        },
-        {
-          type: 'Control',
-          scope: '#/properties/recurrence',
-        },
-      ],
-    },
-  ],
-}
-
-const initialData = {}
 export function Item() {
   const { schema, uiSchema, item } = useLoaderData()
   const [data, setData] = useState(item)
