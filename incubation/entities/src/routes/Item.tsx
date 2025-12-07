9b89685a20a65cf986102e09e372135637f46cdf
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-12-07 22:12:18 +0100

    Basic item display

--- incubation/entities/src/routes/Item.tsx
+++ incubation/entities/src/routes/Item.tsx
@@ -1,7 +1,7 @@
 import { JsonForms } from '@jsonforms/react'
 import { vanillaCells, vanillaRenderers } from '@jsonforms/vanilla-renderers'
 import { useState } from 'react'
-import { useLoaderData } from 'react-router'
+import { useLoaderData, type LoaderFunction } from 'react-router'
 
 const schema = {
   type: 'object',
@@ -54,17 +54,38 @@ const uischema = {
 
 const initialData = {}
 export function Item() {
-  console.log('useLoaderData', useLoaderData())
-  const { item } = useLoaderData()
-  const [data, setData] = useState(initialData)
+  const { schema, uiSchema, item } = useLoaderData()
+  const [data, setData] = useState(item)
   return (
     <JsonForms
       schema={schema}
-      uischema={uischema}
+      uischema={uiSchema}
       data={data}
       renderers={vanillaRenderers}
       cells={vanillaCells}
       onChange={({ data }) => setData(data)}
     />
   )
+}
+
+export const itemLoader: LoaderFunction = async ({ params }) => {
+  const [schema, uiSchema, data] = await Promise.all([
+    (async () => {
+      const response = await fetch('/schema/book-collection.schema.json')
+      const schema = await response.json()
+      return schema
+    })(),
+    (async () => {
+      const response = await fetch('/ui-schema/book-item.uischema.json')
+      const uiSchema = await response.json()
+      return uiSchema
+    })(),
+    (async () => {
+      const response = await fetch('/data/book-collection.data.json')
+      const data: any[] = await response.json()
+      return data
+    })(),
+  ])
+  console.log('itemLoader', { params, schema, uiSchema, data })
+  return { schema, uiSchema, item: data[Number.parseInt(params.id!)] }
 }
--- incubation/entities/src/routes/index.tsx
+++ incubation/entities/src/routes/index.tsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from 'react-router'
 import { Collection } from './Collection.tsx'
-import { Item } from './Item.tsx'
+import { Item, itemLoader } from './Item.tsx'
 
 export const router = createBrowserRouter([
   {
     path: '/:id',
     element: <Item />,
-    loader: async ({params}) => ({ item: params.id }),
+    loader: itemLoader,
   },
   {
     path: '/',
