fdba305f963cb0065e153b01d53eea51e9744307
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-12-12 19:47:57 +0100

    Basic mantine setup

--- incubation/entities/src/main.tsx
+++ incubation/entities/src/main.tsx
@@ -1,11 +1,15 @@
+import { createTheme, MantineProvider } from '@mantine/core'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import './index.css'
 import { router } from './routes/index.tsx'
+export const theme = createTheme({})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <MantineProvider theme={theme}>
+      <RouterProvider router={router} />
+    </MantineProvider>
   </StrictMode>
 )
