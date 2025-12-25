061c06514e2d5297d54a25fa9f9916bf23ca0250
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-12-25 20:38:10 +0100

    Using local cells

--- incubation/mantine-renderers/src/index.ts
+++ incubation/mantine-renderers/src/index.ts
@@ -1,6 +1,45 @@
 import type { RankedTester } from '@jsonforms/core'
-import { vanillaCells, vanillaRenderers } from '@jsonforms/vanilla-renderers'
+import { vanillaRenderers } from '@jsonforms/vanilla-renderers'
+import {
+  BooleanCell,
+  booleanCellTester,
+  DateCell,
+  dateCellTester,
+  DateTimeCell,
+  dateTimeCellTester,
+  EnumCell,
+  enumCellTester,
+  IntegerCell,
+  integerCellTester,
+  NumberCell,
+  numberCellTester,
+  NumberFormatCell,
+  numberFormatCellTester,
+  OneOfEnumCell,
+  oneOfEnumCellTester,
+  SliderCell,
+  sliderCellTester,
+  TextAreaCell,
+  textAreaCellTester,
+  TextCell,
+  textCellTester,
+  TimeCell,
+  timeCellTester,
+} from './cells'
 
-export const mantineCells: { tester: RankedTester; cell: any }[] = [...vanillaCells]
+export const mantineCells: { tester: RankedTester; cell: any }[] = [
+  { tester: booleanCellTester, cell: BooleanCell },
+  { tester: dateCellTester, cell: DateCell },
+  { tester: dateTimeCellTester, cell: DateTimeCell },
+  { tester: enumCellTester, cell: EnumCell },
+  { tester: integerCellTester, cell: IntegerCell },
+  { tester: numberCellTester, cell: NumberCell },
+  { tester: numberFormatCellTester, cell: NumberFormatCell },
+  { tester: oneOfEnumCellTester, cell: OneOfEnumCell },
+  { tester: sliderCellTester, cell: SliderCell },
+  { tester: textAreaCellTester, cell: TextAreaCell },
+  { tester: textCellTester, cell: TextCell },
+  { tester: timeCellTester, cell: TimeCell },
+]
 
 export const mantineRenderers: { tester: RankedTester; renderer: any }[] = [...vanillaRenderers]
