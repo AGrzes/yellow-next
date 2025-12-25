f40f6d0c7a5ea42b45748c707f9c8e718d013759
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-12-25 19:17:45 +0100

    Number Cell

---
+++ incubation/mantine-renderers/src/cells/NumberCell.story.tsx
@@ -0,0 +1,29 @@
+import type { OwnPropsOfCell } from '@jsonforms/core'
+import type { Meta, StoryObj } from '@storybook/react-vite'
+import { NumberCell } from './NumberCell'
+import { makeCellStory } from './story-helpers'
+
+const meta = {
+  component: NumberCell,
+} satisfies Meta<typeof NumberCell>
+
+export default meta
+type Story = StoryObj<OwnPropsOfCell>
+
+const schema = { type: 'number' }
+
+export const Default: Story = makeCellStory(NumberCell, {
+  value: 3.14,
+  schema,
+})
+
+export const Empty: Story = makeCellStory(NumberCell, {
+  value: undefined,
+  schema,
+})
+
+export const Disabled: Story = makeCellStory(NumberCell, {
+  value: 3.14,
+  schema,
+  enabled: false,
+})
