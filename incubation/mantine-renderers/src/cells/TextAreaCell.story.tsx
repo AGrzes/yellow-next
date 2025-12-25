66d5b6da78fca3daf92059981a0a5eb5809768d2
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-12-25 19:18:32 +0100

    Text Area Cell

---
+++ incubation/mantine-renderers/src/cells/TextAreaCell.story.tsx
@@ -0,0 +1,33 @@
+import type { OwnPropsOfCell } from '@jsonforms/core'
+import type { Meta, StoryObj } from '@storybook/react-vite'
+import { TextAreaCell } from './TextAreaCell'
+import { makeCellStory } from './story-helpers'
+
+const meta = {
+  component: TextAreaCell,
+} satisfies Meta<typeof TextAreaCell>
+
+export default meta
+type Story = StoryObj<OwnPropsOfCell>
+
+const schema = { type: 'string' }
+const uischemaOptions = { multi: true, placeholder: 'Write a few lines...' }
+
+export const Default: Story = makeCellStory(TextAreaCell, {
+  value: 'Line one\nLine two',
+  schema,
+  uischemaOptions,
+})
+
+export const Empty: Story = makeCellStory(TextAreaCell, {
+  value: undefined,
+  schema,
+  uischemaOptions,
+})
+
+export const Disabled: Story = makeCellStory(TextAreaCell, {
+  value: 'Read-only text',
+  schema,
+  uischemaOptions,
+  enabled: false,
+})
---
+++ incubation/mantine-renderers/src/cells/TextAreaCell.tsx
@@ -0,0 +1,1 @@
+export { TextAreaCell } from '@jsonforms/vanilla-renderers'
