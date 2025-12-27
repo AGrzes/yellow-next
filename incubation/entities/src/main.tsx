4032d670f3dad7fffd6d74914294bd4880120bb3
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-12-27 21:03:04 +0100

    Mantine styles

--- incubation/entities/src/main.tsx
+++ incubation/entities/src/main.tsx
@@ -1,4 +1,6 @@
 import { createTheme, MantineProvider } from '@mantine/core'
+import '@mantine/core/styles.css'
+import '@mantine/dates/styles.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
