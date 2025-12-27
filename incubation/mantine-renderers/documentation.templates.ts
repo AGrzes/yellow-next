2c08a567e2d37a457ce0e0e796def8f72bfe982f
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-12-27 12:09:30 +0100

    Documetation templates

---
+++ incubation/mantine-renderers/documentation.templates.ts
@@ -0,0 +1,41 @@
+// Control/Cell Documentation 
+/**
+ * {{Name of main component}}
+ * 
+ * {{Short description of functinality}}
+ * 
+ * Implementing: {{Link to JSON Forms documentation}}
+ * {{#if exist}}
+ * Inspired By: {{Link to vanilla-forms documentation in form of URL to github not local path}}
+ * {{/if}}
+ * {{#if applicable}}
+ * Deviations: 
+ * {{Description of deviations from JsonForms expectations}}
+ * {{/if}}
+ * {{#if applicable}}
+ * Implementation Notes:
+ * {{Description of non obvious decisions taken during implementation like logical split shared components etc, not mention things copied as is from vanilla}}
+ * {{/if}}
+ */
+
+// Aggregate module documentation
+/**
+ * {{Name of group}}
+ * 
+ * {{Short description of functinality}}
+ * Implementing: {{Link to JSON Forms documentation}}
+ * Includes:
+ * {{#each components}}
+ * - {{Name of component}}: {{Short description of component functionality}}
+ * {{/each}}
+ * {{#if applicable}}
+ * Deviations: {{Description of deviations from JsonForms expectations from specific components}}
+ * {{/if}}
+ * 
+ */ 
+// Helper module documentation
+/**
+ * {{Name of module}}
+ * 
+ * {{Short description of functinality}}
+ */
