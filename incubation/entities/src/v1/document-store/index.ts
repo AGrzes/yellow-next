c496eaf75db9bdec783a50cda2711ed7134da15d
Grzegorz Abramczyk <abramczykg@gmail.com>
2026-01-14 20:21:38 +0100

    Updated DocumentSotre api

--- incubation/entities/src/v1/document-store/index.ts
+++ incubation/entities/src/v1/document-store/index.ts
@@ -1,20 +1,25 @@
-export type DocumentKey = string | string[]
-export type Revision = string
+/**
+ * Hierarchical key made up of string-identified container IDs and the final document ID.
+ */
+export type DocumentKey = string[]
+/**
+ * Part of hierarchical key made up of string-identified container IDs.
+ */
+export type DocumentKeyPrefix = string[]
+/**
+ * Store specific revision identifier for optimistic concurrency control.
+ */
 
-export interface Document<Body> {
+export interface Document<Body, Revision> {
   key: DocumentKey
   body: Body
   revision: Revision
 }
 
-export interface DocumentStore {
-  get<Body>(key: DocumentKey): Promise<Document<Body> | null>
+export interface DocumentStore<Revision = string> {
+  get<Body>(key: DocumentKey): Promise<Document<Body, Revision> | null>
 
-  list<Body>(startKey: DocumentKey, endKey: DocumentKey): Promise<Document<Body>[]>
+  list<Body>(prefix?: DocumentKeyPrefix): Promise<Document<Body, Revision>[]>
 
-  merge<Body>(
-    key: DocumentKey,
-    merge: (current: Document<Body> | null) => Body,
-    expectedRevision?: Revision
-  ): Promise<void>
+  merge<Body>(document: Document<Body, Revision>, merge: (document: Body, current?: Body) => Body): Promise<void>
 }
