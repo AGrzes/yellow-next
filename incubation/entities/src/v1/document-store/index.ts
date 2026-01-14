6a6070c4ca982c95af91918627b0bf313dec5705
Grzegorz Abramczyk <abramczykg@gmail.com>
2026-01-14 20:25:57 +0100

    Updated DocumentSotre api

--- incubation/entities/src/v1/document-store/index.ts
+++ incubation/entities/src/v1/document-store/index.ts
@@ -7,19 +7,55 @@ export type DocumentKey = string[]
  */
 export type DocumentKeyPrefix = string[]
 /**
- * Store specific revision identifier for optimistic concurrency control.
+ * Document stored in DocumentStore.
  */
-
 export interface Document<Body, Revision> {
+  /**
+   * Document key.
+   */
   key: DocumentKey
+  /**
+   * Document body.
+   */
   body: Body
+  /**
+   * Document revision for optimistic concurrency control.
+   */
   revision: Revision
 }
+/**
+ * Function that merges document body.
+ * @param document - Document body to merge
+ * @param current - Current document body, or undefined if it not yet exist
+ * @returns Merged document body
+ */
+export type MergeFunction<Body> = (document: Body, current?: Body) => Body
 
+/**
+ * Interface for a hierarchical document store supporting optimistic concurrency control.
+ */
 export interface DocumentStore<Revision = string> {
+  /**
+   * Returns specific document by its key.
+   *
+   * @param key - Document key
+   * @returns The document with the specified key, or null if it does not exist.
+   */
   get<Body>(key: DocumentKey): Promise<Document<Body, Revision> | null>
 
+  /**
+   * Returns list of documents inside container specified by the prefix and nested containers.
+   *
+   * @param prefix - Document key prefix
+   * @returns List of documents
+   */
   list<Body>(prefix?: DocumentKeyPrefix): Promise<Document<Body, Revision>[]>
 
-  merge<Body>(document: Document<Body, Revision>, merge: (document: Body, current?: Body) => Body): Promise<void>
+  /**
+   * Merges document body using the provided merge function.
+   *
+   * @param document - Document to merge
+   * @param merge - Merge function
+   */
+  merge<Body>(document: Document<Body, Revision>, merge: MergeFunction<Body>): Promise<void>
 }
