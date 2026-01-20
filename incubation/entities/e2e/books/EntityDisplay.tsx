90c6595eba410885345344095c7d527553135afe
Grzegorz Abramczyk <abramczykg@gmail.com>
2026-01-20 12:22:08 +0100

    Working book storybook

--- incubation/entities/e2e/books/EntityDisplay.tsx
+++ incubation/entities/e2e/books/EntityDisplay.tsx
@@ -1,11 +1,18 @@
 import { Entity } from '../../src/v1/entity/index.ts'
+import { mantineCells, mantineRenderers } from '@agrzes/mantine-renderers'
+import { JsonForms } from '@jsonforms/react'
 
 export function EntityDisplay({ entity }: { entity: Entity<any> }) {
   return (
     <div>
       <h2>Entity Type: {entity.type}</h2>
       <h3>Entity ID: {entity.id}</h3>
-      <pre>{JSON.stringify(entity.body, null, 2)}</pre>
+      <JsonForms
+        schema={entity.meta?.type?.schema}
+        data={entity.body}
+        renderers={mantineRenderers}
+        cells={mantineCells}
+      />
     </div>
   )
 }
