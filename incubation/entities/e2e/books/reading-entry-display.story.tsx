0d46c3dafc30c4843da82342a7e5e86ae734f7dd
Grzegorz Abramczyk <abramczykg@gmail.com>
2026-01-20 12:46:08 +0100

    Storybooks for separate entities

---
+++ incubation/entities/e2e/books/reading-entry-display.story.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+import { EntityDisplay } from './EntityDisplay.tsx'
+
+import { collections } from './data-browser.ts'
+import { schemas } from './schema-browser.ts'
+import { setupEntityManager } from './setup-entity-manager.ts'
+
+const entityManager = await setupEntityManager(collections, schemas)
+
+const meta = {
+  component: EntityDisplay,
+} satisfies Meta<typeof EntityDisplay>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const ReadingEntry: Story = {
+  args: {
+    entity: (await entityManager.get<any>(
+      collections['reading-entry'].type,
+      collections['reading-entry'].items[0].id
+    ))!,
+  },
+}
