54490ba96d579a3de45dd94a5242bf5a69feb841
Grzegorz Abramczyk <abramczykg@gmail.com>
2026-01-21 22:10:34 +0100

    Entity display cleanup

--- incubation/entities/src/v1/components/entity-display/EntityDisplay.tsx
+++ incubation/entities/src/v1/components/entity-display/EntityDisplay.tsx
@@ -5,16 +5,12 @@ import type { Entity } from '@v1/entity'
 
 export function EntityDisplay({ entity, uiSchema }: { entity: Entity<any>; uiSchema?: UISchemaElement }) {
   return (
-    <div>
-      <h2>Entity Type: {entity.type}</h2>
-      <h3>Entity ID: {entity.id}</h3>
-      <JsonForms
-        schema={entity.meta?.type?.schema}
-        data={entity.body}
-        uischema={uiSchema}
-        renderers={mantineRenderers}
-        cells={mantineCells}
-      />
-    </div>
+    <JsonForms
+      schema={entity.meta?.type?.schema}
+      data={entity.body}
+      uischema={uiSchema}
+      renderers={mantineRenderers}
+      cells={mantineCells}
+    />
   )
 }
